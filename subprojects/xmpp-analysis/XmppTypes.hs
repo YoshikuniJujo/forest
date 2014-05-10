7a108d35f81bddef67650ff103e9eb60b3de6bf8
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-05-11 04:56:51 +0900

    mechanismをHaskellの型に変換した

--- subprojects/xmpp-analysis/XmppTypes.hs
+++ subprojects/xmpp-analysis/XmppTypes.hs
@@ -7,10 +7,11 @@ module XmppTypes (
 
 import Data.Maybe
 import Data.XML.Types
+import Data.Text (Text)
 
 data Stanza
 	= Auth Mechanism
-	| StanzaMechanisms [Element]
+	| StanzaMechanisms [Mechanism]
 	| StanzaTag Tag Element
 	| StanzaRaw Element
 	deriving Show
@@ -18,17 +19,22 @@ data Stanza
 data Tag
 	= Features
 	| Mechanisms
+	| Mechanism
 	deriving (Show, Eq)
 
 data Mechanism
-	= DigestMd5
+	= ScramSha1
+	| DigestMd5
+	| UnknownMechanism Text
+	| NotMechanism Element
 	deriving Show
 
 elementToStanza :: Element -> Stanza
 elementToStanza (Element nm [] [NodeElement nd@(Element nm' [] nds)])
 	| Just Features <- nameToTag nm,
 		Just Mechanisms <- nameToTag nm' =
-		StanzaMechanisms $ map (fromJust . nodeElementElement) nds
+		StanzaMechanisms $ map
+			(elementToMechanism . fromJust . nodeElementElement) nds
 elementToStanza e@(Element n _ _)
 	| Just t <- nameToTag n = StanzaTag t e
 	| otherwise = StanzaRaw e
@@ -37,15 +43,34 @@ stanzaToElement :: Stanza -> Element
 stanzaToElement (StanzaMechanisms nds) = Element
 	(fromJust $ lookup Features tagName) [] [NodeElement e]
 	where
-	e = Element (fromJust $ lookup Mechanisms tagName) [] $ map NodeElement nds
+	e = Element (fromJust $ lookup Mechanisms tagName) [] $ map NodeElement $
+		map mechanismToElement nds
 stanzaToElement (StanzaTag _ e) = e
 stanzaToElement (StanzaRaw e) = e
+
+elementToMechanism :: Element -> Mechanism
+elementToMechanism e@(Element nm [] [NodeContent (ContentText mn)])
+	| Just Mechanism <- nameToTag nm = case mn of
+		"SCRAM-SHA-1" -> ScramSha1
+		"DIGEST-MD5" -> DigestMd5
+		_ -> UnknownMechanism mn
+	| otherwise = NotMechanism e
+
+mechanismToElement :: Mechanism -> Element
+mechanismToElement (NotMechanism e) = e
+mechanismToElement m = let Just nm = lookup Mechanism tagName in
+	Element nm [] . (: []) . NodeContent . ContentText $ case m of
+		ScramSha1 -> "SCRAM-SHA-1"
+		DigestMd5 -> "DIGEST-MD5"
+		UnknownMechanism mn -> mn
 
 tagName :: [(Tag, Name)]
 tagName = [
 	(Features, Name "features"
 		(Just "http://etherx.jabber.org/streams") (Just "stream")),
 	(Mechanisms, Name "mechanisms"
+		(Just "urn:ietf:params:xml:ns:xmpp-sasl") Nothing),
+	(Mechanism, Name "mechanism"
 		(Just "urn:ietf:params:xml:ns:xmpp-sasl") Nothing)
  ]
 
