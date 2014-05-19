9d14b0614299f043b39b67309ea6989da5f24416
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-05-19 17:40:39 +0900

    #42 サーバのHandleを取って内容を取得するhttpClient関数を作成した

---
+++ subprojects/http-analysis/Client.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
+
+module Client (httpClient) where
+
+import Control.Applicative
+import Data.Maybe
+import System.IO
+
+import HttpTypes
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+
+httpClient :: Handle -> IO BS.ByteString
+httpClient sv = do
+	BSC.hPutStrLn sv request
+	src <- hGetHeader sv
+	let res = parseResponse src
+	cnt <- BS.hGet sv (contentLength $ responseContentLength res)
+	let res' = res { responseBody = cnt }
+	mapM_ BSC.putStrLn . catMaybes $ showResponse res'
+	return cnt
+
+hGetHeader :: Handle -> IO [BS.ByteString]
+hGetHeader h = do
+	l <- dropCR <$> BSC.hGetLine h
+	if (BS.null l) then return [] else (l :) <$> hGetHeader h
+
+dropCR :: BS.ByteString -> BS.ByteString
+dropCR s = if BSC.last s == '\r' then BS.init s else s
+
+crlf :: [BS.ByteString] -> BS.ByteString
+crlf = BS.concat . map (+++ "\r\n")
+
+request :: BS.ByteString
+request = crlf . catMaybes . showRequest . RequestGet (Uri "/") (Version 1 1) $
+	Get {
+		getHost = Just . Host "localhost" $ Just 8080,
+		getUserAgent = Just [Product "Mozilla" (Just "5.0")],
+		getAccept = Just [Accept ("text", "plain") (Qvalue 1.0)],
+		getAcceptLanguage = Just [AcceptLanguage "ja" (Qvalue 1.0)],
+		getAcceptEncoding = Just [],
+		getConnection = Just [Connection "close"],
+		getCacheControl = Just [MaxAge 0],
+		getOthers = []
+	 }
--- subprojects/http-analysis/client.hs
+++ subprojects/http-analysis/client.hs
@@ -1,50 +1,12 @@
 {-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 
-import Control.Applicative
-import Data.Maybe
-import System.IO
 import System.Environment
 import Network
 
-import HttpTypes
-
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
+import Client
 
 main :: IO ()
 main = do
 	(pn :: Int) : _ <- mapM readIO =<< getArgs
 	sv <- connectTo "localhost" . PortNumber $ fromIntegral pn
-	BS.putStr $ request
-	BSC.hPutStrLn sv request
-	src <- hGetHeader sv
-	mapM_ BSC.putStrLn src
-	let res = parseResponse src
-	print res
-	BS.hGet sv (contentLength $ responseContentLength res) >>= print
-	putStrLn ""
---	replicateM_ 15 $ hGetLine sv >>= print
-
-hGetHeader :: Handle -> IO [BS.ByteString]
-hGetHeader h = do
-	l <- dropCR <$> BSC.hGetLine h
-	if (BS.null l) then return [] else (l :) <$> hGetHeader h
-
-dropCR :: BS.ByteString -> BS.ByteString
-dropCR s = if BSC.last s == '\r' then BS.init s else s
-
-crlf :: [BS.ByteString] -> BS.ByteString
-crlf = BS.concat . map (+++ "\r\n")
-
-request :: BS.ByteString
-request = crlf . catMaybes . showRequest . RequestGet (Uri "/") (Version 1 1) $
-	Get {
-		getHost = Just . Host "localhost" $ Just 8080,
-		getUserAgent = Just [Product "Mozilla" (Just "5.0")],
-		getAccept = Just [Accept ("text", "plain") (Qvalue 1.0)],
-		getAcceptLanguage = Just [AcceptLanguage "ja" (Qvalue 1.0)],
-		getAcceptEncoding = Just [],
-		getConnection = Just [Connection "close"],
-		getCacheControl = Just [MaxAge 0],
-		getOthers = []
-	 }
+	httpClient sv >>= print
