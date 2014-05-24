8aad1edcd89aafb04ed9d04dc37e50c62efdfdce
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-05-24 14:32:48 +0900

    #60 TLSサーバのContent.hsをすこしリファクタリングした

--- subprojects/tls-analysis/server/Content.hs
+++ subprojects/tls-analysis/server/Content.hs
@@ -1,99 +1,49 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Content (
-	Content(..), fragmentToContent, contentToFragment, contentListToFragment,
-	Handshake(..), ClientHello(..), ServerHello(..), SessionId(..),
-	CertificateRequest(..), ClientCertificateType(..),
-	HashAlgorithm(..), SignatureAlgorithm(..),
-	applicationData,
-	showHandshake,
+	Content(..), getContent, contentToFragment, contentListToFragment,
+	ChangeCipherSpec(..),
 
-	EncryptedPreMasterSecret(..),
-
-	Version(..), Random(..),
-	doesServerHelloDone,
-
-	clientVersion, clientRandom,
-	makeVerify,
-	makeClientKeyExchange,
-	makeClientHello,
-
-	serverVersion, serverRandom, serverCipherSuite, getFinish,
-	getCertificateRequest,
-
+	Handshake(..),
+	ClientHello(..), ServerHello(..),
+	Version(..), Random(..), SessionId(..),
 	CipherSuite(..), CompressionMethod(..),
-
-	DigitallySigned(..), ChangeCipherSpec(..),
-
-	getContent,
+	EncryptedPreMasterSecret(..),
+	CertificateRequest(..),
+	ClientCertificateType(..), HashAlgorithm(..), SignatureAlgorithm(..),
+	DigitallySigned(..),
 ) where
 
-import Prelude hiding (concat, head)
-
 import Control.Monad
-import Control.Applicative
-
-import Handshake
-import Data.ByteString(ByteString, pack, concat)
-import qualified Data.ByteString as BS
 import Data.Word
+import qualified Data.ByteString as BS
+import Handshake
 import Types
 
-showHandshake :: Content -> String
-showHandshake (ContentHandshake _ hs) = show hs
-showHandshake _ = ""
+getContent :: Monad m => (Int -> m BS.ByteString) -> ContentType -> m Content
+getContent rd ct = parseContent rd ct (Version 3 3)
 
-makeClientHello :: Random -> Content
-makeClientHello cr = ContentHandshake (Version 3 3) . HandshakeClientHello $
-	ClientHello (Version 3 3) cr (SessionId "")
-		[TLS_RSA_WITH_AES_128_CBC_SHA]
-		[CompressionMethodNull]
-		Nothing
-
-applicationData :: ByteString -> Content
-applicationData = ContentApplicationData (Version 3 3)
-
-fragmentToContent :: Fragment -> Either String [Content]
-fragmentToContent (Fragment ct v body) = evalByteStringM (parseContent ct v) body
-
-getContent :: Monad m =>
-	(Int -> m BS.ByteString) -> ContentType -> m Content
-getContent rd ct = parseContent' rd ct (Version 3 3)
-
-parseContent' :: Monad m =>
+parseContent :: Monad m =>
 	(Int -> m BS.ByteString) -> ContentType -> Version -> m Content
-parseContent' rd ContentTypeChangeCipherSpec v =
+parseContent rd ContentTypeChangeCipherSpec v =
 	ContentChangeCipherSpec v `liftM` parse' rd
-parseContent' rd ContentTypeAlert v = do
+parseContent rd ContentTypeAlert v = do
 	[al, ad] <- BS.unpack `liftM` rd 2
 	return $ ContentAlert v al ad
-parseContent' rd ContentTypeHandshake v = ContentHandshake v `liftM` parse' rd
-parseContent' _ ContentTypeApplicationData _ = undefined
-parseContent' _ _ _ = undefined -- ContentRaw ct v <$> whole
-
-parseContent :: ContentType -> Version -> ByteStringM [Content]
-parseContent ContentTypeChangeCipherSpec v =
-	map (ContentChangeCipherSpec v) <$> parse
-parseContent ContentTypeAlert v = do
-	al <- headBS
-	ad <- headBS
-	return [ContentAlert v al ad]
-parseContent ContentTypeHandshake v =
-	map (ContentHandshake v) <$> parse
-parseContent ContentTypeApplicationData v =
-	(: []) . ContentApplicationData v <$> whole
-parseContent ct v = (: []) . ContentRaw ct v <$> whole
+parseContent rd ContentTypeHandshake v = ContentHandshake v `liftM` parse' rd
+parseContent _ ContentTypeApplicationData _ = undefined
+parseContent _ _ _ = undefined
 
 contentListToFragment :: [Content] -> Fragment
 contentListToFragment cs = let
 	fs@(Fragment ct vsn _ : _) = map contentToFragment cs in
-	Fragment ct vsn . concat $ map (\(Fragment _ _ b) -> b) fs
+	Fragment ct vsn . BS.concat $ map (\(Fragment _ _ b) -> b) fs
 
 contentToFragment :: Content -> Fragment
 contentToFragment (ContentChangeCipherSpec v ccs) =
 	Fragment ContentTypeChangeCipherSpec v $ changeCipherSpecToByteString ccs
 contentToFragment (ContentAlert v al ad) =
-	Fragment ContentTypeAlert v $ pack [al, ad]
+	Fragment ContentTypeAlert v $ BS.pack [al, ad]
 contentToFragment (ContentHandshake v hss) = Fragment ContentTypeHandshake v $
 	toByteString hss
 contentToFragment (ContentApplicationData v body) =
@@ -104,13 +54,9 @@ data Content
 	= ContentChangeCipherSpec Version ChangeCipherSpec
 	| ContentAlert Version Word8 Word8
 	| ContentHandshake Version Handshake
-	| ContentApplicationData Version ByteString
-	| ContentRaw ContentType Version ByteString
+	| ContentApplicationData Version BS.ByteString
+	| ContentRaw ContentType Version BS.ByteString
 	deriving Show
-
-doesServerHelloDone :: Content -> Bool
-doesServerHelloDone (ContentHandshake _ HandshakeServerHelloDone) = True
-doesServerHelloDone _ = False
 
 data ChangeCipherSpec
 	= ChangeCipherSpec
@@ -139,41 +85,6 @@ parseChangeCipherSpec' rd = do
 		1 -> ChangeCipherSpec
 		_ -> ChangeCipherSpecRaw ccs
 
-changeCipherSpecToByteString :: ChangeCipherSpec -> ByteString
-changeCipherSpecToByteString ChangeCipherSpec = pack [1]
-changeCipherSpecToByteString (ChangeCipherSpecRaw ccs) = pack [ccs]
-
-makeVerify :: ByteString -> Content
-makeVerify = ContentHandshake (Version 3 3) . handshakeMakeVerify
-
-clientRandom :: Content -> Maybe Random
-clientRandom (ContentHandshake _ hss) = handshakeClientRandom hss
-clientRandom _ = Nothing
-
-clientVersion :: Content -> Maybe Version
-clientVersion (ContentHandshake _ hss) = handshakeClientVersion hss
-clientVersion _ = Nothing
-
-makeClientKeyExchange :: EncryptedPreMasterSecret -> Content
-makeClientKeyExchange =
-	ContentHandshake (Version 3 3) . handshakeMakeClientKeyExchange
-
-serverVersion :: Content -> Maybe Version
-serverVersion (ContentHandshake _ hs) = handshakeServerVersion hs
-serverVersion _ = Nothing
-
-serverRandom :: Content -> Maybe Random
-serverRandom (ContentHandshake _ hs) = handshakeServerRandom hs
-serverRandom _ = Nothing
-
-serverCipherSuite :: Content -> Maybe CipherSuite
-serverCipherSuite (ContentHandshake _ hs) = handshakeCipherSuite hs
-serverCipherSuite _ = Nothing
-
-getFinish :: Content -> Maybe ByteString
-getFinish (ContentHandshake _ hs) = handshakeGetFinish hs
-getFinish _ = Nothing
-
-getCertificateRequest :: Content -> Maybe CertificateRequest
-getCertificateRequest (ContentHandshake _ hs) = handshakeCertificateRequest hs
-getCertificateRequest _ = Nothing
+changeCipherSpecToByteString :: ChangeCipherSpec -> BS.ByteString
+changeCipherSpecToByteString ChangeCipherSpec = BS.pack [1]
+changeCipherSpecToByteString (ChangeCipherSpecRaw ccs) = BS.pack [ccs]
