e0e6ecfe6760c0fec389d2b37c2d3b712ecf401c
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-05-30 03:46:20 +0900

    #95 自作のTLSクライアントとDHE_RSAで通信できるサーバを作成した

---
+++ subprojects/tls-analysis/server_dhe/Types.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Types (
+	Fragment(..),
+	Version(..), byteStringToVersion, versionToByteString,
+	ContentType(..), byteStringToContentType, contentTypeToByteString,
+	Random(..), CipherSuite(..), CipherSuiteKeyEx(..), CipherSuiteMsgEnc(..),
+) where
+
+import Data.Word
+import qualified Data.ByteString as BS
+
+data Fragment
+	= Fragment ContentType Version BS.ByteString
+	deriving Show
+
+data Version
+	= Version Word8 Word8
+	deriving (Show, Eq, Ord)
+
+byteStringToVersion :: BS.ByteString -> Version
+byteStringToVersion v = let [vmjr, vmnr] = BS.unpack v in Version vmjr vmnr
+
+versionToByteString :: Version -> BS.ByteString
+versionToByteString (Version vmjr vmnr) = BS.pack [vmjr, vmnr]
+
+data ContentType
+	= ContentTypeChangeCipherSpec
+	| ContentTypeAlert
+	| ContentTypeHandshake
+	| ContentTypeApplicationData
+	| ContentTypeRaw Word8
+	deriving (Show, Eq)
+
+byteStringToContentType :: BS.ByteString -> ContentType
+byteStringToContentType "\20" = ContentTypeChangeCipherSpec
+byteStringToContentType "\21" = ContentTypeAlert
+byteStringToContentType "\22" = ContentTypeHandshake
+byteStringToContentType "\23" = ContentTypeApplicationData
+byteStringToContentType bs = let [ct] = BS.unpack bs in ContentTypeRaw ct
+
+contentTypeToByteString :: ContentType -> BS.ByteString
+contentTypeToByteString ContentTypeChangeCipherSpec = BS.pack [20]
+contentTypeToByteString ContentTypeAlert = BS.pack [21]
+contentTypeToByteString ContentTypeHandshake = BS.pack [22]
+contentTypeToByteString ContentTypeApplicationData = BS.pack [23]
+contentTypeToByteString (ContentTypeRaw ct) = BS.pack [ct]
+
+data Random = Random BS.ByteString
+
+data CipherSuiteKeyEx
+	= RSA
+	| DHE_RSA
+	| ECDHE_PSK
+	| KeyExNULL
+	deriving (Show, Eq)
+
+data CipherSuiteMsgEnc
+	= AES_128_CBC_SHA
+	| AES_128_CBC_SHA256
+	| CAMELLIA_128_CBC_SHA
+	| NULL_SHA
+	| MsgEncNULL
+	deriving (Show, Eq)
+
+data CipherSuite
+	= CipherSuite CipherSuiteKeyEx CipherSuiteMsgEnc
+	| CipherSuiteRaw Word8 Word8
+	deriving (Show, Eq)
