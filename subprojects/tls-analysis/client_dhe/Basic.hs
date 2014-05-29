417d8465393850f6b772f62f373028473e0c5689
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-05-30 03:41:30 +0900

    #97 TLSクライアントでのTLS_DHE_RSAへの対応
    apache2とのTLS_DHE_RSA_WITH_AES_128_CBC_SHAによる接続に成功した。

---
+++ subprojects/tls-analysis/client_dhe/Basic.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Basic (
+	Fragment(..),
+	Version(..), byteStringToVersion, versionToByteString,
+	ContentType(..), byteStringToContentType, contentTypeToByteString,
+	Random(..), CipherSuite(..),
+	showKey, showKeySingle, word64ToByteString,
+	byteStringToInt, intToByteString,
+	lenBodyToByteString, fromInt, fst3,
+) where
+
+import Data.Bits
+import Data.Word
+import Data.ByteString (ByteString, pack, unpack)
+import qualified Data.ByteString as BS
+import Numeric
+
+data Fragment
+	= Fragment ContentType Version ByteString
+	deriving Show
+
+data Version
+	= Version Word8 Word8
+	deriving Show
+
+byteStringToVersion :: ByteString -> Version
+byteStringToVersion v = let [vmjr, vmnr] = unpack v in Version vmjr vmnr
+
+versionToByteString :: Version -> ByteString
+versionToByteString (Version vmjr vmnr) = pack [vmjr, vmnr]
+
+data ContentType
+	= ContentTypeChangeCipherSpec
+	| ContentTypeAlert
+	| ContentTypeHandshake
+	| ContentTypeApplicationData
+	| ContentTypeRaw Word8
+	deriving Show
+
+byteStringToContentType :: ByteString -> ContentType
+byteStringToContentType "\20" = ContentTypeChangeCipherSpec
+byteStringToContentType "\21" = ContentTypeAlert
+byteStringToContentType "\22" = ContentTypeHandshake
+byteStringToContentType "\23" = ContentTypeApplicationData
+byteStringToContentType bs = let [ct] = unpack bs in ContentTypeRaw ct
+
+contentTypeToByteString :: ContentType -> ByteString
+contentTypeToByteString ContentTypeChangeCipherSpec = pack [20]
+contentTypeToByteString ContentTypeAlert = pack [21]
+contentTypeToByteString ContentTypeHandshake = pack [22]
+contentTypeToByteString ContentTypeApplicationData = pack [23]
+contentTypeToByteString (ContentTypeRaw ct) = pack [ct]
+
+data Random = Random ByteString
+
+data CipherSuite
+	= TLS_NULL_WITH_NULL_NULL
+	| TLS_RSA_WITH_AES_128_CBC_SHA
+	| TLS_DHE_RSA_WITH_AES_128_CBC_SHA
+	| TLS_ECDHE_PSK_WITH_NULL_SHA
+	| TLS_RSA_WITH_AES_128_CBC_SHA256
+	| TLS_DHE_RSA_WITH_CAMELLIA_128_CBC_SHA
+	| CipherSuiteRaw Word8 Word8
+	deriving (Show, Eq)
+
+showKeySingle :: ByteString -> String
+showKeySingle = unwords . map showH . BS.unpack
+
+showKey :: ByteString -> String
+showKey = init . unlines . map unwords . separateN 16 . map showH . BS.unpack
+	where
+	separateN _ [] = []
+	separateN n xs = Prelude.take n xs : separateN n (drop n xs)
+
+showH :: Word8 -> String
+showH w = let s = showHex w "" in replicate (2 - length s) '0' ++ s
+
+word64ToByteString :: Word64 -> ByteString
+word64ToByteString w64 = BS.replicate (8 - BS.length bs) 0 `BS.append` bs
+	where
+	bs = BS.reverse $ wtb w64
+	wtb 0 = ""
+	wtb w = fromIntegral (w .&. 0xff) `BS.cons` wtb (w `shiftR` 8)
+
+byteStringToInt :: ByteString -> Int
+byteStringToInt bs = wordsToInt (BS.length bs - 1) $ BS.unpack bs
+
+wordsToInt :: Int -> [Word8] -> Int
+wordsToInt n _ | n < 0 = 0
+wordsToInt _ [] = 0
+wordsToInt n (x : xs) = fromIntegral x `shift` (n * 8) .|. wordsToInt (n - 1) xs
+
+intToByteString :: Int -> Int -> ByteString
+intToByteString n = BS.pack . reverse . intToWords n
+
+intToWords :: Int -> Int -> [Word8]
+intToWords 0 _ = []
+intToWords n i = fromIntegral i : intToWords (n - 1) (i `shiftR` 8)
+
+lenBodyToByteString :: Int -> ByteString -> ByteString
+lenBodyToByteString n bs = intToByteString n (BS.length bs) `BS.append` bs
+
+fromInt :: Integral i => Int -> i
+fromInt = fromIntegral
+
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
---
+++ subprojects/tls-analysis/client_dhe/TlsClient.hs
@@ -0,0 +1,270 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+
+module TlsClient (
+	TlsServer,
+	openTlsServer, tPut, tGetByte, tGetLine, tGet, tGetContent, tClose,
+
+	Option(..),
+	tPutWithCT,
+	Content(..),
+	ContentType(..),
+	Handshake(..),
+	Version(..),
+) where
+
+import System.IO
+import Control.Applicative
+import Control.Monad
+import Data.List
+import Data.Word
+import qualified Data.ByteString as BS
+import Data.X509
+import Data.X509.CertificateStore
+import Data.X509.Validation
+import Crypto.PubKey.RSA
+
+import Fragment
+import Content
+import Basic
+
+import Crypto.PubKey.DH
+
+import Data.Ratio
+
+openTlsServer :: [(PrivateKey, CertificateChain)] -> CertificateStore -> Handle
+	-> [Option] -> IO TlsServer
+openTlsServer ccs certStore sv opts = runOpen (handshake ccs certStore opts) sv
+
+isIncluded :: (PrivateKey, CertificateChain) -> [DistinguishedName] -> Bool
+isIncluded (_, CertificateChain certs) dns = let
+	idn = certIssuerDN . signedObject . getSigned $ last certs in
+	idn `elem` dns
+
+helloVersionFromOptions :: [Option] -> (Word8, Word8)
+helloVersionFromOptions =
+	maybe (3, 3) (\(OptHelloVersion mjr mnr) -> (mjr, mnr)) .
+		find isOptHelloVersion
+
+clientVersionFromOptions :: [Option] -> (Word8, Word8)
+clientVersionFromOptions =
+	maybe (3, 3) (\(OptClientVersion mjr mnr) -> (mjr, mnr)) .
+		find isOptClientVersion
+
+handshake :: [(PrivateKey, CertificateChain)] -> CertificateStore
+	-> [Option] -> TlsIo Content ()
+handshake ccs certStore opts = do
+
+	-------------------------------------------
+	--     CLIENT HELLO                      --
+	-------------------------------------------
+	cr <- Random <$> randomByteString 32
+	let ch = clientHello cr
+		(helloVersionFromOptions opts)
+		(clientVersionFromOptions opts)
+		(if OptEmptyCipherSuite `elem` opts then [] else [
+			TLS_DHE_RSA_WITH_AES_128_CBC_SHA,
+			TLS_RSA_WITH_AES_128_CBC_SHA256,
+			TLS_RSA_WITH_AES_128_CBC_SHA])
+		(if OptEmptyCompressionMethod `elem` opts
+			then []
+			else [CompressionMethodNull])
+	case (OptStartByChangeCipherSpec `elem` opts,
+		OptStartByFinished `elem` opts) of
+		(True, _) -> writeContent changeCipherSpec
+		(_, True) -> writeContent $ ContentHandshake version $
+			HandshakeFinished ""
+		_ -> writeContent ch
+	fragmentUpdateHash $ contentToFragment ch
+	maybe (throwError "No Client Hello") setClientRandom $ clientRandom ch
+
+	-------------------------------------------
+	--     SERVER HELLO                      --
+	-------------------------------------------
+	sh <- readContent
+	liftIO $ print sh
+	maybe (throwError "No Server Hello") setVersion $ serverVersion sh
+	maybe (throwError "No Server Hello") setServerRandom $
+		serverRandom sh
+	maybe (throwError "No Server Hello") cacheCipherSuite $
+		serverCipherSuite sh
+	liftIO . putStrLn $ "SERVER HELLO: " ++ take 60 (show sh) ++ "..."
+
+	-------------------------------------------
+	--     SERVER CERTIFICATE                --
+	-------------------------------------------
+	crt <- readContent
+	let	Just scc@(CertificateChain (cert : _)) = certificateChain crt
+		PubKeyRSA pub = certPubKey $ getCertificate cert
+	v <- liftIO $ validateDefault certStore
+		(ValidationCache query add) ("localhost", "localhost da") scc
+	liftIO . putStrLn $ "VALIDATE RESULT: " ++ show v
+	unless (null v) $ throwError "SERVER VALIDATION FAILURE"
+	liftIO . putStrLn $ "CERTIFICATE: " ++ take 60 (show crt) ++ "..."
+	liftIO . putStrLn $ "CERTIFICATE Chain: " ++ take 60 (show scc) ++ "..."
+	liftIO . putStrLn $ "PUBKEY: " ++ take 60 (show pub) ++ "..."
+
+	-------------------------------------------
+	--     CERTIFICATE REQUEST               --
+	-------------------------------------------
+	-------------------------------------------
+	--     SERVER HELLO DONE                 --
+	-------------------------------------------
+	(crtReq, yc, dhsk) <- serverHelloDone pub
+
+	let	Just (CertificateRequest _ _ sdn) = crtReq
+		(pk, cc) = head $ filter (`isIncluded` sdn) ccs
+
+	-------------------------------------------
+	--     CLIENT CERTIFICATE                --
+	-------------------------------------------
+	case crtReq of
+		Just _ -> do
+			writeContent $ if OptNotClientCertificate `elem` opts
+				then ContentHandshake version $
+					HandshakeFinished ""
+				else certificate cc
+			fragmentUpdateHash . contentToFragment $ certificate cc
+		_ -> return ()
+
+	-------------------------------------------
+	--     CLIENT KEY EXCHANGE               --
+	-------------------------------------------
+--	let pmsVer = if OptPmsVerErr `elem` opts then "\x99\x99" else "\x03\x03"
+--	pms <- (pmsVer `BS.append`) <$> randomByteString 46
+--	epms' <- encryptRSA pub pms
+--	liftIO $ putStrLn $ "Encrypted Pre Master Secret: " ++ show epms'
+	generateKeys dhsk -- pms
+--	let	cke'' = makeClientKeyExchange $ EncryptedPreMasterSecret epms'
+	let	cke'' = makeClientKeyExchange $ EncryptedPreMasterSecret yc
+	writeContent $ if OptNotClientKeyExchange `elem` opts
+		then ContentHandshake version $ HandshakeFinished ""
+		else cke''
+	fragmentUpdateHash $ contentToFragment cke''
+--	liftIO $ putStrLn $ "KEY EXCHANGE: " ++ show (contentToFragment cke'')
+	liftIO $ putStrLn "GENERATE KEYS"
+
+--	debugPrintKeys
+
+	-------------------------------------------
+	--     CERTIFICATE VERIFY                --
+	-------------------------------------------
+	case crtReq of
+		Just _ -> do
+			signed <- clientVerifySign pk $ OptBadSignature `elem` opts
+			let	(ha, sa) = if OptNotExistHashAndSignature `elem` opts
+					then (HashAlgorithmRaw 255,
+						SignatureAlgorithmRaw 255)
+					else (HashAlgorithmSha256,
+						SignatureAlgorithmRsa)
+				cv = if OptNotCertificateVerify `elem` opts
+					then ContentHandshake version $
+						HandshakeFinished ""
+					else makeVerify ha sa signed
+			writeContent cv
+			fragmentUpdateHash . contentToFragment $ cv
+		_ -> return ()
+
+	-------------------------------------------
+	--     CLIENT CHANGE CIPHER SPEC         --
+	-------------------------------------------
+	writeContent changeCipherSpec
+	fragmentUpdateHash $ contentToFragment changeCipherSpec
+	flushCipherSuite Client
+
+	-------------------------------------------
+	--     CLIENT FINISHED                   --
+	-------------------------------------------
+	fhc <- finishedHash Client
+	liftIO . putStrLn $ "CLIENT FINISHED: " ++ show fhc
+	writeContent $ finished fhc
+	fragmentUpdateHash . contentToFragment $ finished fhc
+
+	liftIO $ putStrLn "CLIENT FINISHED DONE"
+
+	-------------------------------------------
+	--     SERVER CHANGE CIPHER SPEC         --
+	-------------------------------------------
+	sccs <- readContent
+	when (doesChangeCipherSpec sccs) $ flushCipherSuite Server
+	liftIO $ print sccs
+	when (isFatal sccs) . error $ show sccs
+	liftIO . putStrLn $ "SERVER CHANGE CIPHER SPEC: " ++ take 60 (show sccs)
+
+	-------------------------------------------
+	--     SERVER FINISHED                   --
+	-------------------------------------------
+	sfhc <- finishedHash Server
+	scf <- readContent
+	updateSequenceNumberSmart Server
+	sfinish <- maybe (throwError $ "Not Finished: " ++ show scf)
+		return $ getFinish scf
+	liftIO $ do
+		putStrLn $ "SERVER FINISHED FIREFOX     : " ++ take 60 (show sfinish)
+		putStrLn $ "SERVER FINISHED CALCULATE   : " ++ take 60 (show sfhc)
+
+serverHelloDone :: PublicKey -> TlsIo Content (Maybe CertificateRequest, BS.ByteString, BS.ByteString)
+serverHelloDone pub = do
+
+	-------------------------------------------
+	--     CERTIFICATE REQUEST               --
+	-------------------------------------------
+	crtReq <- readContent
+	liftIO . putStrLn $
+--		"CERTIFICATE REQUEST: " ++ take 60 (show crtReq) ++ "..."
+		"CERTIFICATE REQUEST: " ++ show crtReq
+
+	let	ContentHandshake _ (HandshakeServerKeyExchange ske) = crtReq
+		ServerKeyExchange ps ys _ _ _ _ = ske
+
+	cr <- getClientRandom
+	sr <- getServerRandom
+	liftIO . print $ verifyServerKeyExchange pub cr sr ske
+
+	g <- getRandomGen
+	let	(pr, g') = generatePrivate g ps
+		dhsk = getShared ps pr ys
+	setRandomGen g'
+	liftIO . putStrLn $ "PRIVATE NUMBER: " ++ show pr
+	liftIO . putStrLn $ "SHARED KEY    : " ++ show dhsk
+
+	unless (doesServerHelloDone crtReq) $ do
+
+	-------------------------------------------
+	--     SERVER HELLO DONE                 --
+	-------------------------------------------
+		shd <- readContent
+		liftIO . putStrLn $ "SERVER HELLO DONE: " ++ take 60 (show shd) ++ "..."
+
+	return $ (getCertificateRequest crtReq,
+		integerToByteString $ toInteger $ calculatePublic ps pr,
+--		integerToByteString $ toInteger (calculatePublic ps pr) + 1,
+		integerToByteString $ numerator $ toRational dhsk)
+
+readContent :: TlsIo Content Content
+readContent = do
+	c <- readCached readContentList
+	fragmentUpdateHash $ contentToFragment c
+	return c
+
+readContentList :: TlsIo Content [Content]
+readContentList = (\ec -> case ec of
+		Right c -> c;
+		Left err -> error $ "readContentList: " ++ err) .
+	fragmentToContent <$> readFragment
+
+_writeContentList :: Partner -> [Content] -> TlsIo Content ()
+_writeContentList partner cs = do
+	let f = contentListToFragment cs
+	updateSequenceNumberSmart partner
+	writeFragment f
+
+writeContent :: Content -> TlsIo Content ()
+writeContent c = do
+	let f = contentToFragment c
+	writeFragment f
+
+query :: ValidationCacheQueryCallback
+query _ _ _ = return ValidationCacheUnknown
+
+add :: ValidationCacheAddCallback
+add _ _ _ = return ()
