a119dcd48f5aea07b3ddfb3e4f9873441ea76521
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-06-02 12:49:04 +0900

    #109 作りかけのECDHE用のサーバをコミット
    まずはクライアントとTLS_ECDHE_RSA_WITH_AES_128_RSA256で接続できるようにする。

---
+++ subprojects/tls-analysis/server_ecdhe/KeyExchange.hs
@@ -0,0 +1,203 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module KeyExchange (
+	ServerKeyExchange(..),
+	verifyServerKeyExchange,
+	integerToByteString,
+	secp256r1,
+	encodePoint,
+	EcCurveType(..),
+) where
+
+import GHC.Real
+
+import Control.Applicative
+import Control.Arrow
+import ByteStringMonad
+import qualified Data.ByteString as BS
+
+import qualified Crypto.PubKey.RSA as RSA
+import qualified Crypto.PubKey.RSA.Prim as RSA
+import qualified Crypto.Hash.SHA1 as SHA1
+
+import Data.ASN1.Encoding
+import Data.ASN1.BinaryEncoding
+import Data.ASN1.Types
+import Data.ASN1.Error
+
+import Data.Bits
+
+-- import Crypto.PubKey.DH
+
+import Parts
+import Crypto.Types.PubKey.ECC
+
+secp256r1 :: Curve
+secp256r1 = CurveFP $ CurvePrime p (CurveCommon a b g n h)
+	where
+	p = 0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff
+	a = 0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc
+	b = 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b
+	g = Point gx gy
+	gx = 0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296
+	gy = 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5
+	n = 0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551
+	h = 0x01
+
+verifyServerKeyExchange :: RSA.PublicKey -> BS.ByteString -> BS.ByteString ->
+	ServerKeyExchange -> (BS.ByteString, Either ASN1Error [ASN1])
+	{-
+verifyServerKeyExchange pub cr sr ske@(ServerKeyExchange _ps _ys _ha _sa s "") =
+	let	body = BS.concat $ [cr, sr, getBody ske]
+		hash = SHA1.hash body
+		unSign = BS.tail . BS.dropWhile (/= 0) . BS.drop 2 $ RSA.ep pub s in
+		(hash, decodeASN1' BER unSign)
+		-}
+verifyServerKeyExchange pub cr sr ske@(ServerKeyExchangeEc _ _ _ _ _ _ s "") =
+	let	body = BS.concat $ [cr, sr, getBody ske]
+		hash = SHA1.hash body
+		unSign = BS.tail . BS.dropWhile (/= 0) . BS.drop 2 $ RSA.ep pub s in
+		(hash, decodeASN1' BER unSign)
+	
+verifyServerKeyExchange _ _ _ _ = error "verifyServerKeyExchange: bad"
+
+getBody :: ServerKeyExchange -> BS.ByteString
+{-
+getBody (ServerKeyExchange (Params p g) ys _ha _sa _ "") =
+	BS.concat $ map (lenBodyToByteString 2) [
+		BS.pack $ toWords p,
+		BS.pack $ toWords g,
+		BS.pack $ toWords $ fromIntegral ys ]
+		-}
+getBody (ServerKeyExchangeEc ct nc t p ha sa _sign "") =
+	BS.concat $ [
+		toByteString ct,
+		toByteString nc,
+		lenBodyToByteString 1 $ encodePoint t p]
+getBody _ = error "bad"
+
+data ServerKeyExchange
+{-
+	= ServerKeyExchange Params PublicNumber
+		Word8 Word8 BS.ByteString
+		BS.ByteString
+		-}
+	= ServerKeyExchangeEc EcCurveType NamedCurve Word8 Point
+		Word8 Word8 BS.ByteString
+		BS.ByteString
+	| ServerKeyExchangeRaw BS.ByteString
+	deriving Show
+
+encodePoint :: Word8 -> Point -> BS.ByteString
+encodePoint t (Point x y) =
+	t `BS.cons` integerToByteString x `BS.append` integerToByteString y
+
+decodePoint :: BS.ByteString -> (Word8, Point)
+decodePoint bs = case BS.uncons bs of
+	Just (t, rest) -> let (x, y) = BS.splitAt 32 rest in
+		(t, Point (toI $ BS.unpack x) (toI $ BS.unpack y))
+	_ -> error "KeyExchange:readPoint"
+
+instance Parsable ServerKeyExchange where
+	parse = parseServerKeyExchange
+	toByteString = serverKeyExchangeToByteString
+	listLength _ = Nothing
+
+parseServerKeyExchange :: ByteStringM ServerKeyExchange
+parseServerKeyExchange = do
+	ct <- parse
+	nc <- parse
+	ecp <- takeLen 1
+	let (t, p) = decodePoint ecp
+	ha <- headBS
+	sa <- headBS
+	sign <- takeLen 2
+	rest <- whole
+	return $ ServerKeyExchangeEc ct nc t p ha sa sign rest
+{- do
+	(dhPl, dhP) <- toI . BS.unpack <$> takeLen 2
+	(dhGl, dhG) <- toI . BS.unpack <$> takeLen 2
+	(dhYsl, dhYs) <- toI . BS.unpack <$> takeLen 2
+	hashA <- headBS
+	sigA <- headBS
+	sign <- takeLen 2
+	rest <- whole
+	return $ ServerKeyExchange
+		(Params dhP dhG) (fromInteger dhYs) hashA sigA sign rest
+		-}
+
+serverKeyExchangeToByteString :: ServerKeyExchange -> BS.ByteString
+{-
+serverKeyExchangeToByteString
+	(ServerKeyExchange (Params dhP dhG) dhYs hashA sigA sign rest) =
+	BS.concat [
+		lenBodyToByteString 2 . BS.pack $ toWords dhP,
+		lenBodyToByteString 2 . BS.pack $ toWords dhG,
+		lenBodyToByteString 2 . BS.pack . toWords $ fromIntegral dhYs]
+	`BS.append`
+	BS.pack [hashA, sigA] `BS.append`
+	BS.concat [lenBodyToByteString 2 sign, rest]
+	-}
+serverKeyExchangeToByteString ske@(ServerKeyExchangeEc _ _ _ _ ha sa sn rst) =
+	BS.concat [
+		getBody ske,
+		BS.pack [ha, sa],
+		sn,
+		rst
+	 ]
+serverKeyExchangeToByteString (ServerKeyExchangeRaw bs) = bs
+
+toI :: [Word8] -> Integer
+toI ws = wordsToInteger $ reverse ws
+
+wordsToInteger :: [Word8] -> Integer
+wordsToInteger [] = 0
+wordsToInteger (w : ws) = fromIntegral w .|. (wordsToInteger ws `shiftL` 8)
+
+toWords :: Integer -> [Word8]
+toWords = reverse . integerToWords
+
+integerToWords :: Integer -> [Word8]
+integerToWords 0 = []
+integerToWords i = fromIntegral i : integerToWords (i `shiftR` 8)
+
+{-
+instance Integral PublicNumber where
+	toInteger pn = case (numerator $ toRational pn, denominator $ toRational pn) of
+		(i, 1) -> i
+		_ -> error "bad"
+	quotRem pn1 pn2 = fromInteger *** fromInteger $
+		toInteger pn1 `quotRem` toInteger pn2
+
+instance Integral PrivateNumber where
+	toInteger pn = case (numerator $ toRational pn, denominator $ toRational pn) of
+		(i, 1) -> i
+		_ -> error "bad"
+	quotRem pn1 pn2 = fromInteger *** fromInteger $
+		toInteger pn1 `quotRem` toInteger pn2
+		-}
+
+integerToByteString :: Integer -> BS.ByteString
+integerToByteString = BS.pack . toWords
+
+data EcCurveType
+	= ExplicitPrime
+	| ExplicitChar2
+	| NamedCurve
+	| EcCurveTypeRaw Word8
+	deriving Show
+
+instance Parsable EcCurveType where
+	parse = do
+		w <- headBS
+		return $ case w of
+			1 -> ExplicitPrime
+			2 -> ExplicitChar2
+			3 -> NamedCurve
+			_ -> EcCurveTypeRaw w
+	toByteString ExplicitPrime = BS.pack [1]
+	toByteString ExplicitChar2 = BS.pack [2]
+	toByteString NamedCurve = BS.pack [3]
+	toByteString (EcCurveTypeRaw w) = BS.pack [w]
+	listLength _ = Nothing
---
+++ subprojects/tls-analysis/server_ecdhe/TlsServer.hs
@@ -0,0 +1,340 @@
+{-# LANGUAGE OverloadedStrings, PackageImports #-}
+
+module TlsServer (
+	TlsClient, openClient, withClient, checkName, getName,
+	readRsaKey, readCertificateChain, readCertificateStore
+) where
+
+import System.IO.Unsafe
+import "crypto-random" Crypto.Random
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Exception
+import Data.Maybe
+import Data.HandleLike
+import Data.ASN1.Types
+import Data.X509
+import Data.X509.File
+import Data.X509.Validation
+import Data.X509.CertificateStore
+import System.IO
+import Content
+import Fragment
+
+import Crypto.Types.PubKey.ECC
+
+import qualified Data.ByteString as BS
+import qualified Crypto.PubKey.RSA as RSA
+import qualified Crypto.PubKey.RSA.Prim as RSA
+
+import Crypto.PubKey.DH
+
+import Types
+
+version :: Version
+version = Version 3 3
+
+sessionId :: SessionId
+sessionId = SessionId ""
+
+cipherSuite :: [CipherSuite] -> CipherSuite
+cipherSuite css
+	| CipherSuite ECDHE_RSA AES_128_CBC_SHA `elem` css =
+		CipherSuite ECDHE_RSA AES_128_CBC_SHA
+	| CipherSuite DHE_RSA AES_128_CBC_SHA256 `elem` css =
+		CipherSuite DHE_RSA AES_128_CBC_SHA256
+	| CipherSuite DHE_RSA AES_128_CBC_SHA `elem` css =
+		CipherSuite DHE_RSA AES_128_CBC_SHA
+	| CipherSuite RSA AES_128_CBC_SHA256 `elem` css =
+		CipherSuite RSA AES_128_CBC_SHA256
+	| otherwise = CipherSuite RSA AES_128_CBC_SHA
+
+compressionMethod :: CompressionMethod
+compressionMethod = CompressionMethodNull
+
+clientCertificateType :: ClientCertificateType
+clientCertificateType = ClientCertificateTypeRsaSign
+
+clientCertificateAlgorithm :: (HashAlgorithm, SignatureAlgorithm)
+clientCertificateAlgorithm = (HashAlgorithmSha256, SignatureAlgorithmRsa)
+
+validationCache :: ValidationCache
+validationCache = ValidationCache
+	(\_ _ _ -> return ValidationCacheUnknown)
+	(\_ _ _ -> return ())
+
+validationChecks :: ValidationChecks
+validationChecks = defaultChecks{ checkFQHN = False }
+
+openClient :: Handle
+	-> RSA.PrivateKey -> CertificateChain -> Maybe CertificateStore
+	-> IO TlsClient
+openClient h = ((runOpen h .) .) . handshake
+
+withClient :: Handle
+	-> RSA.PrivateKey -> CertificateChain -> Maybe CertificateStore
+	-> (TlsClient -> IO a) -> IO a
+withClient = (((flip bracket hlClose .) .) .) . openClient
+
+handshake :: RSA.PrivateKey -> CertificateChain -> Maybe CertificateStore
+	-> TlsIo [String]
+handshake sk cc mcs = do
+	(cv, css) <- clientHello
+	serverHello sk css cc mcs
+	mpn <- maybe (return Nothing) ((Just <$>) . clientCertificate) mcs
+	clientKeyExchange sk cv
+	maybe (return ()) (certificateVerify . fst) mpn
+	clientChangeCipherSuite
+	clientFinished
+	serverChangeCipherSuite
+	serverFinished
+	return $ maybe [] snd mpn
+
+clientHello :: TlsIo (Version, [CipherSuite])
+clientHello = do
+	hs <- readHandshake $ \(Version mj _) -> mj == 3
+	liftIO $ print hs
+	case hs of
+		HandshakeClientHello (ClientHello vsn rnd _ css cms _) ->
+			err vsn css cms >> setClientRandom rnd >> return (vsn, css)
+		_ -> throwError $ Alert AlertLevelFatal
+			AlertDescriptionUnexpectedMessage
+			"TlsServer.clientHello: not client hello"
+	where
+	err vsn css cms
+		| vsn < version = throwError $ Alert
+			AlertLevelFatal AlertDescriptionProtocolVersion
+			"TlsServer.clientHello: client version should 3.3 or more"
+		| CipherSuite ECDHE_RSA AES_128_CBC_SHA256 `notElem` css = throwError $ Alert
+			AlertLevelFatal AlertDescriptionIllegalParameter
+			"TlsServer.clientHello: no supported cipher suites"
+		| compressionMethod `notElem` cms = throwError $ Alert
+			AlertLevelFatal AlertDescriptionDecodeError
+			"TlsServer.clientHello: no supported compression method"
+		| otherwise = return ()
+
+(dhParams, dhPrivate) = unsafePerformIO $ do
+	g <- cprgCreate <$> createEntropyPool :: IO SystemRNG
+	let	(ps, g') = generateParams g 512 2
+--	let	(ps, g') = generateParams g 128 2
+		(pr, g'') = generatePrivate g' ps
+	return (ps, pr)
+	
+
+serverHello :: RSA.PrivateKey -> [CipherSuite] -> CertificateChain ->
+	Maybe CertificateStore -> TlsIo ()
+serverHello pk css cc mcs = do
+	sr <- randomByteString 32
+	Just cr <- getClientRandom
+--	g <- getRandomGen
+--	let	(ps, g') = generateParams g 8 2
+--		(pr, g'') = generatePrivate g' ps
+	let	ske = HandshakeServerKeyExchange $ -- addSign pk cr sr $
+			ServerKeyExchangeEc
+				NamedCurve
+				Secp256r1
+				4
+				(Point 888 888)
+				10
+				15
+				"\x00\x05 defg"
+				""
+			{-
+				dhParams (calculatePublic dhParams dhPrivate)
+				2 1 "hogeru" ""
+				-}
+	liftIO $ print ske
+	liftIO $ print $ contentToFragment $ ContentHandshake version ske
+	liftIO . putStrLn $ "CIPHER SUITE: " ++ show (cipherSuite css)
+--	setRandomGen g''
+	setVersion version
+	setServerRandom $ Random sr
+	cacheCipherSuite $ cipherSuite css
+	((>>) <$> writeFragment <*> fragmentUpdateHash) . contentListToFragment .
+		map (ContentHandshake version) $ catMaybes [
+		Just $ HandshakeServerHello $ ServerHello version (Random sr)
+			sessionId
+			(cipherSuite css) compressionMethod Nothing,
+		Just $ HandshakeCertificate cc,
+		Just $ ske,
+		case mcs of
+			Just cs -> Just $ HandshakeCertificateRequest
+				. CertificateRequest
+					[clientCertificateType]
+					[clientCertificateAlgorithm]
+				. map (certIssuerDN . signedObject . getSigned)
+				$ listCertificates cs
+			_ -> Nothing,
+		Just HandshakeServerHelloDone]
+
+clientCertificate :: CertificateStore -> TlsIo (RSA.PublicKey, [String])
+clientCertificate cs = do
+	hs <- readHandshake (== version)
+	case hs of
+		HandshakeCertificate cc@(CertificateChain (c : _)) ->
+			case certPubKey $ getCertificate c of
+				PubKeyRSA pub -> chk cc >> return (pub, names cc)
+				p -> throwError $ Alert AlertLevelFatal
+					AlertDescriptionUnsupportedCertificate
+					("TlsServer.clientCertificate: " ++
+						"not implemented: " ++ show p)
+		_ -> throwError $ Alert AlertLevelFatal
+			AlertDescriptionUnexpectedMessage
+			"TlsServer.clientCertificate: not certificate"
+	where
+	chk cc = do
+		rs <- liftIO $ validate HashSHA256 defaultHooks validationChecks
+			cs validationCache ("", "") cc
+		unless (null rs) . throwError $ Alert AlertLevelFatal
+			(selectAlert rs)
+			("TlsServer.clientCertificate: Validate Failure: "
+				++ show rs)
+	selectAlert rs
+		| Expired `elem` rs = AlertDescriptionCertificateExpired
+		| InFuture `elem` rs = AlertDescriptionCertificateExpired
+		| UnknownCA `elem` rs = AlertDescriptionUnknownCa
+		| otherwise = AlertDescriptionCertificateUnknown
+	names cc = maybe [] (: ans (crt cc)) $ cn (crt cc) >>= asn1CharacterToString
+	cn = getDnElement DnCommonName . certSubjectDN
+	ans = maybe [] (\(ExtSubjectAltName ns) -> mapMaybe uan ns)
+		. extensionGet . certExtensions
+	crt cc = case cc of
+		CertificateChain (t : _) -> getCertificate t
+		_ -> error "TlsServer.clientCertificate: empty certificate chain"
+	uan (AltNameDNS s) = Just s
+	uan _ = Nothing
+
+clientKeyExchange :: RSA.PrivateKey -> Version -> TlsIo ()
+clientKeyExchange sk (Version cvmjr cvmnr) = do
+	hs <- readHandshake (== version)
+	case hs of
+		HandshakeClientKeyExchange (EncryptedPreMasterSecret epms) -> do
+			liftIO $ putStrLn $ "CLIENT KEY: " ++ show epms
+			let pms = ""
+--			let pms = getShared dhParams dhPrivate $
+--				byteStringToPublicNumber epms
+--			generateKeys $ integerToByteString $ fromIntegral pms
+			return ()
+			{-
+			r <- randomByteString 46
+			pms <- mkpms epms `catchError` const (return $ dummy r)
+			generateKeys pms
+			-}
+		_ -> throwError $ Alert AlertLevelFatal
+			AlertDescriptionUnexpectedMessage
+			"TlsServer.clientKeyExchange: not client key exchange"
+	where
+	dummy r = cvmjr `BS.cons` cvmnr `BS.cons` r
+	mkpms epms = do
+		pms <- decryptRSA sk epms
+		unless (BS.length pms == 48) $ throwError "bad: length"
+		case BS.unpack $ BS.take 2 pms of
+			[pmsvmjr, pmsvmnr] -> do
+				unless (pmsvmjr == cvmjr && pmsvmnr == cvmnr) $
+					throwError "bad: version"
+			_ -> throwError "bad: never occur"
+		return pms
+
+certificateVerify :: RSA.PublicKey -> TlsIo ()
+certificateVerify pub = do
+	hash0 <- clientVerifyHash pub
+	hs <- readHandshake (== version)
+	case hs of
+		HandshakeCertificateVerify (DigitallySigned a s) -> do
+			chk a
+			let hash1 = RSA.ep pub s
+			unless (hash1 == hash0) . throwError $ Alert
+				AlertLevelFatal
+				AlertDescriptionDecryptError
+				"client authentification failed"
+		_ -> throwError $ Alert
+			AlertLevelFatal
+			AlertDescriptionUnexpectedMessage
+			"Not Certificate Verify"
+	where
+	chk a = case a of
+		(HashAlgorithmSha256, SignatureAlgorithmRsa) -> return ()
+		_ -> throwError $ Alert
+			AlertLevelFatal
+			AlertDescriptionDecodeError
+			("Not implement such algorithm: " ++ show a)
+
+clientChangeCipherSuite :: TlsIo ()
+clientChangeCipherSuite = do
+	cnt <- readContent (== version)
+	case cnt of
+		ContentChangeCipherSpec v ChangeCipherSpec -> do
+			unless (v == version) . throwError $ Alert
+				AlertLevelFatal
+				AlertDescriptionProtocolVersion
+				"bad version"
+			flushCipherSuite Client
+		_ -> throwError $ Alert
+			AlertLevelFatal
+			AlertDescriptionUnexpectedMessage
+			"Not Change Cipher Spec"
+
+clientFinished :: TlsIo ()
+clientFinished = do
+	fhc <- finishedHash Client
+	cnt <- readContent (== version)
+	case cnt of
+		ContentHandshake v (HandshakeFinished f) -> do
+			unless (v == version) . throwError $ Alert
+				AlertLevelFatal
+				AlertDescriptionProtocolVersion
+				"bad version"
+			unless (f == fhc) . throwError $ Alert
+				AlertLevelFatal
+				AlertDescriptionDecryptError $
+				"Finished error:\n\t" ++
+				show f ++ "\n\t" ++
+				show fhc ++ "\n"
+		_ -> throwError $ Alert
+			AlertLevelFatal
+			AlertDescriptionUnexpectedMessage
+			"Not Finished"
+
+serverChangeCipherSuite :: TlsIo ()
+serverChangeCipherSuite = do
+	writeFragment . contentToFragment $
+		ContentChangeCipherSpec version ChangeCipherSpec
+	flushCipherSuite Server
+
+serverFinished :: TlsIo ()
+serverFinished = writeFragment . contentToFragment .
+	ContentHandshake version . HandshakeFinished =<< finishedHash Server
+
+readHandshake :: (Version -> Bool) -> TlsIo Handshake
+readHandshake ck = do
+	cnt <- readContent ck
+	case cnt of
+		ContentHandshake v hs
+			| ck v -> return hs
+			| otherwise -> throwError $ Alert
+				AlertLevelFatal
+				AlertDescriptionProtocolVersion
+				"Not supported layer version"
+		_ -> throwError $ Alert
+			AlertLevelFatal
+			AlertDescriptionUnexpectedMessage $
+			"Not Handshake: " ++ show cnt
+
+readContent :: (Version -> Bool) -> TlsIo Content
+readContent vc = do
+	c <- getContent (readBufferContentType vc) (readByteString (== version))
+		<* updateSequenceNumber Client
+	fragmentUpdateHash $ contentToFragment c
+	return c
+
+readCertificateChain :: FilePath -> IO CertificateChain
+readCertificateChain = (CertificateChain <$>) . readSignedObject
+
+readRsaKey :: FilePath -> IO RSA.PrivateKey
+readRsaKey fp = do [PrivKeyRSA sk] <- readKeyFile fp; return sk
+
+readCertificateStore :: [FilePath] -> IO CertificateStore
+readCertificateStore fps =
+	makeCertificateStore . concat <$> mapM readSignedObject fps
