79aee947a9ee63fafcd1f8acad50a0199e19e6ff
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-06-13 06:10:46 +0900

    #224 mkTestFile.hsをすこしリファクタリングした

--- subprojects/tls-analysis/server/mkTestFile.hs
+++ subprojects/tls-analysis/server/mkTestFile.hs
@@ -1,49 +1,45 @@
-{-# LANGUAGE OverloadedStrings, PackageImports, ScopedTypeVariables,
- 	TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies, PackageImports #-}
 
 module Main (main) where
 
+import Control.Applicative ((<$>))
 import Control.Monad (forever, void)
 import "monads-tf" Control.Monad.State (liftIO)
 import Control.Concurrent (forkIO)
+import Data.Ratio (numerator)
+import Data.HandleLike (HandleLike(..))
+import Data.Time (UTCTime(..), getCurrentTime, toModifiedJulianDay)
+import System.IO (Handle, IOMode(..), BufferMode(..), openFile, hSetBuffering)
 import System.Environment (getArgs)
+import System.FilePath ((</>), (<.>))
+import System.Directory (createDirectoryIfMissing)
+import System.Posix.Process (getProcessID)
 import Network (listenOn, accept)
 import "crypto-random" Crypto.Random (CPRG(..))
-import MyServer (server, ValidateHandle(..))
-import CommandLine (readCommandLine)
 
-import Data.HandleLike
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
-import System.IO
-
-import System.FilePath
-
-import Data.Time
 import qualified Data.ByteString.Base64 as BASE64
-import System.Posix.Process
-import Data.Ratio
-import Crypto.Hash.SHA256 as SHA256
+import qualified Crypto.Hash.SHA256 as SHA256
 
-import Random
-
-import System.Directory
+import MyServer (server, ValidateHandle(..))
+import CommandLine (readCommandLine)
+import Random (StdGen)
 
 main :: IO ()
 main = do
-	(port, css, tstd, rsa, ec, mcs) <- readCommandLine =<< getArgs
-	soc <- listenOn port
-	let g0 :: StdGen = cprgCreate undefined
-	createDirectoryIfMissing False tstd
+	(prt, css, td, rsa, ec, mcs) <- readCommandLine =<< getArgs
+	soc <- listenOn prt
+	let g = cprgCreate undefined :: StdGen
+	createDirectoryIfMissing False td
 	void . forever $ do
 		(h, _, _) <- liftIO $ accept soc
 		hSetBuffering h NoBuffering
-		fp <- liftIO $ getName tstd
-		print fp
+		fp <- liftIO $ (td </>) <$> createName
 		writeFile (fp <.> "css") $ show css ++ "\n"
 		cl <- openFile (fp <.> "clt") WriteMode
 		sv <- openFile (fp <.> "srv") WriteMode
-		liftIO . forkIO $ server (DebugHandle h cl sv) g0 css rsa ec mcs
+		liftIO . forkIO $ server (DebugHandle h cl sv) g css rsa ec mcs
 
 data DebugHandle = DebugHandle Handle Handle Handle deriving Show
 
@@ -62,22 +58,20 @@ instance HandleLike DebugHandle where
 instance ValidateHandle DebugHandle where
 	validate (DebugHandle h _ _) = validate h
 
-getName :: FilePath -> IO FilePath
-getName tstd = do
+createName :: IO FilePath
+createName = do
 	now <- getCurrentTime
 	pid <- getProcessID
-	let	day = utctDay now
-		time = utctDayTime now
-		strs = [
-			show $ toModifiedJulianDay day,
-			show . numerator $ toRational time,
-			show pid ]
-	return . (tstd </>) . BSC.unpack . sub '/' '-' . BS.take 12 .
-		BASE64.encode .  SHA256.hash . BS.concat $ map BSC.pack strs
+	let strs = concat [
+		show . toModifiedJulianDay $ utctDay now,
+		show . numerator . toRational $ utctDayTime now,
+		show pid ]
+	return . BSC.unpack . sub '/' '-' . BS.take 12 .
+		BASE64.encode . SHA256.hash $ BSC.pack strs
 
 sub :: Char -> Char -> BS.ByteString -> BS.ByteString
-sub pre post bs
+sub pre pst bs
 	| Just (c, bs') <- BSC.uncons bs = if c == pre
-		then BSC.cons post (sub pre post bs')
-		else BSC.cons c (sub pre post bs')
+		then BSC.cons pst (sub pre pst bs')
+		else BSC.cons c (sub pre pst bs')
 	| otherwise = BS.empty
