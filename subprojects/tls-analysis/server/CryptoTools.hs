46e464ef307a8d7a43f94d526e94d35b67f2f28b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-06-15 19:41:27 +0900

    #250 CryptToolsからTLS 1.2以外のためのコードを削除中

--- subprojects/tls-analysis/server/CryptoTools.hs
+++ subprojects/tls-analysis/server/CryptoTools.hs
@@ -3,13 +3,10 @@
 module CryptoTools (
 	encryptMessage, decryptMessage, hashSha1, hashSha256,
 
-	MSVersion(..),
-	tupleToVersion,
+	MSVersion(..), tupleToVersion,
 	ClientRandom(..), ServerRandom(..),
-	generateMasterSecret, generateKeyBlock, generateFinished,
 
-	generateKeys_,
-	finishedHash_,
+	generateKeys_, finishedHash_,
 ) where
 
 import Data.Bits
@@ -19,7 +16,6 @@ import qualified Data.ByteString as BS
 import "crypto-random" Crypto.Random
 import qualified Crypto.Hash.SHA1 as SHA1
 import qualified Crypto.Hash.SHA256 as SHA256
-import qualified Crypto.Hash.MD5 as MD5
 import Crypto.Cipher.AES
 
 import qualified Codec.Bytable as B
@@ -75,8 +71,7 @@ generateKeys_ :: Int -> BS.ByteString -> BS.ByteString ->
 	BS.ByteString -> Either String
 	(BS.ByteString, BS.ByteString, BS.ByteString, BS.ByteString, BS.ByteString)
 generateKeys_ kl cr sr pms = do
-	let	ms = generateMasterSecret
-			pms (ClientRandom cr) (ServerRandom sr)
+	let	ms = generateMasterSecret pms cr sr
 		ems = generateKeyBlock
 			(ClientRandom cr) (ServerRandom sr) ms $ kl * 2 + 32
 		[cwmk, swmk, cwk, swk] = divide [kl, kl, 16, 16] ems
@@ -89,6 +84,15 @@ generateKeys_ kl cr sr pms = do
 
 finishedHash_ :: Bool -> BS.ByteString -> BS.ByteString -> BS.ByteString
 finishedHash_ = generateFinished TLS12
+
+generateFinished :: MSVersion -> Bool -> Bytes -> Bytes -> Bytes
+generateFinished TLS12 isC ms hash =
+	prfSha256 ms (getFinishedLabel isC `BS.append` hash) 12
+generateFinished _ _ _ _ = error "Not implemented"
+
+getFinishedLabel :: Bool -> BS.ByteString
+getFinishedLabel True = "client finished"
+getFinishedLabel False = "server finished"
 
 decryptMessage :: Hash ->
 	BS.ByteString -> BS.ByteString -> Word64 ->
@@ -116,24 +120,8 @@ type PRF = Bytes -> Bytes -> Int -> Bytes
 data ClientRandom = ClientRandom BS.ByteString deriving Show
 data ServerRandom = ServerRandom BS.ByteString deriving Show
 
-generateMasterSecretTls :: PRF -> Bytes -> ClientRandom -> ServerRandom -> Bytes
-generateMasterSecretTls prf premasterSecret (ClientRandom c) (ServerRandom s) =
-    prf premasterSecret seed 48
-  where seed = BS.concat [ "master secret", c, s ]
-
-generateFinished :: MSVersion -> Bool -> Bytes -> Bytes -> Bytes
-generateFinished TLS10 isC ms hash =
-	prfMd5Sha1 ms (getFinishedLabel isC `BS.append` hash) 12
-generateFinished TLS12 isC ms hash =
-	prfSha256 ms (getFinishedLabel isC `BS.append` hash) 12
-generateFinished _ _ _ _ = error "Not implemented"
-
-getFinishedLabel :: Bool -> BS.ByteString
-getFinishedLabel True = "client finished"
-getFinishedLabel False = "server finished"
-
-generateMasterSecret :: Bytes -> ClientRandom -> ServerRandom -> Bytes
-generateMasterSecret = generateMasterSecretTls prfSha256
+generateMasterSecret :: Bytes -> BS.ByteString -> BS.ByteString -> Bytes
+generateMasterSecret pms c s = prfSha256 pms (BS.concat ["master secret", c, s]) 48
 
 generateKeyBlockTls :: PRF -> ClientRandom -> ServerRandom -> Bytes -> Int -> Bytes
 generateKeyBlockTls prf (ClientRandom c) (ServerRandom s) mastersecret =
@@ -158,12 +146,6 @@ hmac f bl secret msg =
           where kt  = if BS.length secret > fromIntegral bl then f secret else secret
                 pad = BS.replicate (fromIntegral bl - BS.length kt) 0
 
-hmacMD5 :: HMAC
-hmacMD5 = hmac MD5.hash 64
-
-hmacSHA1 :: HMAC
-hmacSHA1 = hmac SHA1.hash 64
-
 hmacSHA256 :: HMAC
 hmacSHA256 = hmac SHA256.hash 64
 
@@ -175,19 +157,6 @@ hmacIter f secret seed aprev len =
     if digestsize >= len
         then [ BS.take (fromIntegral len) out ]
         else out : hmacIter f secret seed an (len - digestsize)
-
-prfSha1 :: BS.ByteString -> BS.ByteString -> Int -> BS.ByteString
-prfSha1 secret seed len = BS.concat $ hmacIter hmacSHA1 secret seed seed len
-
-prfMd5 :: BS.ByteString -> BS.ByteString -> Int -> BS.ByteString
-prfMd5 secret seed len = BS.concat $ hmacIter hmacMD5 secret seed seed len
-
-prfMd5Sha1 :: BS.ByteString -> BS.ByteString -> Int -> BS.ByteString
-prfMd5Sha1 secret seed len =
-    BS.pack $ BS.zipWith xor (prfMd5 s1 seed len) (prfSha1 s2 seed len)
-  where slen  = BS.length secret
-        s1    = BS.take (slen `div` 2 + slen `mod` 2) secret
-        s2    = BS.drop (slen `div` 2) secret
 
 prfSha256 :: BS.ByteString -> BS.ByteString -> Int -> BS.ByteString
 prfSha256 secret seed len = BS.concat $ hmacIter hmacSHA256 secret seed seed len
