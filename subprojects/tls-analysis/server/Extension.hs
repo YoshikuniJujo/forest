9102741a490f223565c4112950061af0e66b0fe7
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-06-24 17:41:20 +0900

    #296 Extensionモジュールのリファクタリング終了: 関数の並び替えを行った

--- subprojects/tls-analysis/server/Extension.hs
+++ subprojects/tls-analysis/server/Extension.hs
@@ -20,7 +20,7 @@ data Extension
 	| ESsnTicketTls BS.ByteString
 	| ENextProtoNego BS.ByteString
 	| ERenegoInfo BS.ByteString
-	| ERaw Type BS.ByteString
+	| ERaw EType BS.ByteString
 	deriving Show
 
 instance B.Bytable Extension where
@@ -28,6 +28,15 @@ instance B.Bytable Extension where
 
 instance B.Parsable Extension where
 	parse = parseE
+
+encodeE :: Extension -> BS.ByteString
+encodeE (ESName sn) = encodeE . ERaw TSName . B.addLen w16 $ cmap B.encode sn
+encodeE (EECurve ec) = encodeE . ERaw TECurve . B.addLen w16 $ cmap B.encode ec
+encodeE (EEcPFrt pf) = encodeE . ERaw TEcPFrt . B.addLen w8 $ cmap B.encode pf
+encodeE (ESsnTicketTls stt) = encodeE $ ERaw TSsnTicketTls stt
+encodeE (ENextProtoNego npn) = encodeE $ ERaw TNextProtoNego npn
+encodeE (ERenegoInfo ri) = encodeE . ERaw TRenegoInfo $ B.addLen w8 ri
+encodeE (ERaw et body) = B.encode et `BS.append` B.addLen w16 body
 
 parseE :: B.BytableM Extension
 parseE = do
@@ -41,26 +50,11 @@ parseE = do
 		TRenegoInfo -> ERenegoInfo <$> (B.take =<< B.take 1)
 		_ -> ERaw t <$> B.take l
 
-encodeE :: Extension -> BS.ByteString
-encodeE (ESName sn) = encodeE . ERaw TSName . B.addLen w16 $ cmap B.encode sn
-encodeE (EECurve ec) = encodeE . ERaw TECurve . B.addLen w16 $ cmap B.encode ec
-encodeE (EEcPFrt pf) = encodeE . ERaw TEcPFrt . B.addLen w8 $ cmap B.encode pf
-encodeE (ESsnTicketTls stt) = encodeE $ ERaw TSsnTicketTls stt
-encodeE (ENextProtoNego npn) = encodeE $ ERaw TNextProtoNego npn
-encodeE (ERenegoInfo ri) = encodeE . ERaw TRenegoInfo $ B.addLen w8 ri
-encodeE (ERaw et body) = B.encode et `BS.append` B.addLen w16 body
-
-w8 :: Word8; w8 = undefined
-w16 :: Word16; w16 = undefined
-
-cmap :: (a -> BS.ByteString) -> [a] -> BS.ByteString
-cmap = (BS.concat .) . map
-
-data Type
+data EType
 	= TSName         | TECurve     | TEcPFrt     | TSsnTicketTls
 	| TNextProtoNego | TRenegoInfo | TRaw Word16 deriving Show
 
-instance B.Bytable Type where
+instance B.Bytable EType where
 	encode TSName = B.encode (0 :: Word16)
 	encode TECurve = B.encode (10 :: Word16)
 	encode TEcPFrt = B.encode (11 :: Word16)
@@ -78,7 +72,7 @@ instance B.Bytable Type where
 				13172 -> TNextProtoNego
 				65281 -> TRenegoInfo
 				et -> TRaw et
-		_ -> Left "Extension: Type.decode"
+		_ -> Left "Extension: EType.decode"
 
 data ServerName = SNHostName BS.ByteString | SNRaw NameType BS.ByteString
 	deriving Show
@@ -100,32 +94,8 @@ instance B.Bytable NameType where
 	encode NTHostName = BS.pack [0]
 	encode (NTRaw t) = BS.pack [t]
 	decode bs = case BS.unpack bs of
-		[t] -> Right $ case t of
-			0 -> NTHostName; _ -> NTRaw t
+		[t] -> Right $ case t of 0 -> NTHostName; _ -> NTRaw t
 		_ -> Left "Extension: NameType.decode"
-
-data EcPointFormat = EPFUncompressed | EPFRaw Word8 deriving Show
-
-instance B.Bytable EcPointFormat where
-	encode EPFUncompressed = BS.pack [0]
-	encode (EPFRaw epf) = BS.pack [epf]
-	decode bs = case BS.unpack bs of
-		[epf] -> Right $ case epf of
-			0 -> EPFUncompressed; _ -> EPFRaw epf
-		_ -> Left "Extension.decodeEPF"
-
-instance B.Bytable ECC.CurveName where
-	encode ECC.SEC_p256r1 = B.encode (23 :: Word16)
-	encode ECC.SEC_p384r1 = B.encode (24 :: Word16)
-	encode ECC.SEC_p521r1 = B.encode (25 :: Word16)
-	encode _ = error "Extension.encodeCN: not implemented"
-	decode bs = case BS.unpack bs of
-		[w1, w2] -> case fromIntegral w1 `shiftL` 8 .|. fromIntegral w2 of
-			(23 :: Word16) -> Right ECC.SEC_p256r1
-			24 -> Right ECC.SEC_p384r1
-			25 -> Right ECC.SEC_p521r1
-			_ -> Left "Extension: CurveName.decode: unknown curve"
-		_ -> Left "Extension: CurveName.decode: bad format"
 
 instance B.Bytable DH.Params where
 	encode (DH.Params p g) =
@@ -135,6 +105,45 @@ instance B.Bytable DH.Params where
 instance B.Bytable DH.PublicNumber where
 	encode = B.addLen w16 . B.encode . \(DH.PublicNumber pn) -> pn
 	decode = B.evalBytableM $ fromInteger <$> (B.take =<< B.take 2)
+
+data EcCurveType = ExplicitPrime | ExplicitChar2 | NamedCurve | ECTRaw Word8
+	deriving Show
+
+instance B.Bytable EcCurveType where
+	encode ExplicitPrime = BS.pack [1]
+	encode ExplicitChar2 = BS.pack [2]
+	encode NamedCurve = BS.pack [3]
+	encode (ECTRaw w) = BS.pack [w]
+	decode = undefined
+
+instance B.Bytable ECC.CurveName where
+	encode ECC.SEC_p256r1 = B.encode (23 :: Word16)
+	encode ECC.SEC_p384r1 = B.encode (24 :: Word16)
+	encode ECC.SEC_p521r1 = B.encode (25 :: Word16)
+	encode _ = error "Extension.encodeCN: not implemented"
+	decode bs = case BS.unpack bs of
+		[w1, w2] -> case fromIntegral w1 `shiftL` 8 .|. fromIntegral w2 of
+			(23 :: Word16) -> Right ECC.SEC_p256r1
+			(24 :: Word16) -> Right ECC.SEC_p384r1
+			(25 :: Word16) -> Right ECC.SEC_p521r1
+			_ -> Left "Extension: CurveName.decode: unknown curve"
+		_ -> Left "Extension: CurveName.decode: bad format"
+
+instance B.Bytable ECC.Curve where
+	encode c
+		| c == ECC.getCurveByName ECC.SEC_p256r1 =
+			B.encode NamedCurve `BS.append` B.encode ECC.SEC_p256r1
+		| otherwise = error "TlsServer.encodeC: not implemented"
+	decode = undefined
+
+data EcPointFormat = EPFUncompressed | EPFRaw Word8 deriving Show
+
+instance B.Bytable EcPointFormat where
+	encode EPFUncompressed = BS.pack [0]
+	encode (EPFRaw f) = BS.pack [f]
+	decode bs = case BS.unpack bs of
+		[f] -> Right $ case f of 0 -> EPFUncompressed; _ -> EPFRaw f
+		_ -> Left "Extension: Bytable.decode"
 
 instance B.Bytable ECC.Point where
 	encode (ECC.Point x y) =
@@ -147,19 +156,8 @@ instance B.Bytable ECC.Point where
 				(either error id $ B.decode y)
 		_ -> Left "Extension: ECC.Point.decode"
 
-data EcCurveType = ExplicitPrime | ExplicitChar2 | NamedCurve | ECTRaw Word8
-	deriving Show
+w8 :: Word8; w8 = undefined
+w16 :: Word16; w16 = undefined
 
-instance B.Bytable EcCurveType where
-	encode ExplicitPrime = BS.pack [1]
-	encode ExplicitChar2 = BS.pack [2]
-	encode NamedCurve = BS.pack [3]
-	encode (ECTRaw w) = BS.pack [w]
-	decode = undefined
-
-instance B.Bytable ECC.Curve where
-	encode c
-		| c == ECC.getCurveByName ECC.SEC_p256r1 =
-			B.encode NamedCurve `BS.append` B.encode ECC.SEC_p256r1
-		| otherwise = error "TlsServer.encodeC: not implemented"
-	decode = undefined
+cmap :: (a -> BS.ByteString) -> [a] -> BS.ByteString
+cmap = (BS.concat .) . map
