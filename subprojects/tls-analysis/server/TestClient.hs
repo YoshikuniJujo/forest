cbc72650a21487642ff2a2c1e0776372bb8be26e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-06-27 22:23:50 +0900

    #324 RSAのほうでhelloの部分とそれ以降の部分とを分けた

--- subprojects/tls-analysis/server/TestClient.hs
+++ subprojects/tls-analysis/server/TestClient.hs
@@ -21,50 +21,64 @@ cipherSuites = [
 	CipherSuite RSA AES_128_CBC_SHA
  ]
 
+hello :: (HandleLike h, CPRG g) =>
+	HandshakeM h g (BS.ByteString, BS.ByteString, CipherSuite)
+hello = do
+	cr <- randomByteString 32
+	writeHandshake $ ClientHello (3, 3) cr (SessionId "")
+		cipherSuites [CompressionMethodNull] Nothing
+	ServerHello _v sr _sid cs _cm _e <- readHandshake
+	return (cr, sr, cs)
+
+rsaHandshake :: (ValidateHandle h, CPRG g) =>
+ 	BS.ByteString -> BS.ByteString ->
+	(RSA.PrivateKey, X509.CertificateChain) ->
+	X509.CertificateStore ->
+	HandshakeM h g ()
+rsaHandshake cr sr (rsk, rcc) crtS = do
+	let X509.PubKeyRSA rcpk = let X509.CertificateChain [rccc] = rcc in
+		X509.certPubKey . X509.signedObject $ X509.getSigned rccc
+	cc@(X509.CertificateChain [ccc]) <- readHandshake
+	shd <- readHandshake
+	cReq <- case shd of
+		Left (CertificateRequest sa hsa dn) -> do
+			ServerHelloDone <- readHandshake
+			return $ Just (sa, hsa, dn)
+		Right ServerHelloDone -> return Nothing
+		_ -> error "bad"
+	handshakeValidate crtS cc >>= debug
+	let X509.PubKeyRSA pk =
+		X509.certPubKey . X509.signedObject $ X509.getSigned ccc
+	pms <- ("\x03\x03" `BS.append`) `liftM` randomByteString 46
+	epms <- encryptRsa pk pms
+	case cReq of
+		Just _ -> writeHandshake rcc
+		_ -> return ()
+	writeHandshake $ Epms epms
+	generateKeys Client (cr, sr) pms
+	hs <- rsaPadding rcpk `liftM` handshakeHash
+	case cReq of
+		Just _ -> writeHandshake $ DigitallySigned (Sha256, Rsa) $
+			RSA.dp Nothing rsk hs
+		_ -> return ()
+	putChangeCipherSpec >> flushCipherSuite Server
+	writeHandshake =<< finishedHash Client
+	getChangeCipherSpec >> flushCipherSuite Client
+	fh <- finishedHash Server
+	rfh <- readHandshake
+	debug $ fh == rfh
+
 client :: (ValidateHandle h, CPRG g) => g -> h ->
 	(RSA.PrivateKey, X509.CertificateChain) ->
 	X509.CertificateStore ->
 	HandleMonad h ()
-client g h (rsk, rcc) crtS = (`run` g) $ do
-	let X509.PubKeyRSA rcpk = let X509.CertificateChain [rccc] = rcc in
-		X509.certPubKey . X509.signedObject $ X509.getSigned rccc
+client g h rsa crtS = (`run` g) $ do
 	t <- execHandshakeM h $ do
-		cr <- randomByteString 32
-		writeHandshake $ ClientHello (3, 3) cr (SessionId "")
-			cipherSuites [CompressionMethodNull] Nothing
-		ServerHello _v sr _sid cs _cm _e <- readHandshake
-		cc@(X509.CertificateChain [ccc]) <- readHandshake
-		shd <- readHandshake -- client certificate
-		cReq <- case shd of
-			Left (CertificateRequest sa hsa dn) -> do
-				ServerHelloDone <- readHandshake
-				return $ Just (sa, hsa, dn)
-			Right ServerHelloDone -> return Nothing
-			_ -> error "bad"
---		debug cReq
+		(cr, sr, cs@(CipherSuite ke _)) <- hello
 		setCipherSuite cs
-		handshakeValidate crtS cc >>= debug
-		debug cs
-		let X509.PubKeyRSA pk =
-			X509.certPubKey . X509.signedObject $ X509.getSigned ccc
-		pms <- ("\x03\x03" `BS.append`) `liftM` randomByteString 46
-		epms <- encryptRsa pk pms
-		case cReq of
-			Just _ -> writeHandshake rcc -- client certificate
-			_ -> return ()
-		writeHandshake $ Epms epms
-		generateKeys Client (cr, sr) pms
-		hs <- rsaPadding rcpk `liftM` handshakeHash
-		case cReq of
-			Just _ -> writeHandshake $ DigitallySigned (Sha256, Rsa) $ RSA.dp Nothing rsk hs -- client certificate
-			_ -> return ()
-		putChangeCipherSpec >> flushCipherSuite Server
-		writeHandshake =<< finishedHash Client
-		getChangeCipherSpec >> flushCipherSuite Client
-		fh <- finishedHash Server
-		rfh <- readHandshake
-		debug $ fh == rfh
-		return ()
+		case ke of
+			RSA -> rsaHandshake cr sr rsa crtS
+			_ -> error "not implemented"
 	hlPut t request
 	hlGetContent t >>= hlDebug t 5
 	return ()
