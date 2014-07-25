8a5b1fc831e87254992aa95aef3ed02d5d079d55
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-07-25 11:19:59 +0900

    #399 xmlEvent関数を作成した

---
+++ subprojects/xml-pipe/XmlEvent.hs
@@ -0,0 +1,22 @@
+module XmlEvent (xmlEvent) where
+
+import Data.Pipe
+import Data.Word8
+import qualified Data.ByteString as BS
+
+import Lexer
+import Papillon
+
+xmlEvent :: Monad m => Pipe BS.ByteString (Maybe XmlEvent) m ()
+xmlEvent = sepTag =$= convert parseXmlEvent =$= filterP (maybe True notEmpty)
+
+notEmpty :: XmlEvent -> Bool
+notEmpty (XECharData cd) = not $ BS.all isSpace cd
+notEmpty _ = True
+
+convert :: Monad m => (a -> b) -> Pipe a b m ()
+convert f = await >>= maybe (return ()) (\x -> yield (f x) >> convert f)
+
+filterP :: Monad m => (a -> Bool) -> Pipe a a m ()
+filterP p = await >>=
+	maybe (return ()) (\x -> (if p x then yield x else return ()) >> filterP p)
--- subprojects/xml-pipe/test.hs
+++ subprojects/xml-pipe/test.hs
@@ -8,33 +8,13 @@ import System.Environment
 
 import qualified Data.ByteString as BS
 
-import Lexer
-import Papillon
+import XmlEvent
 
 main :: IO ()
 main = do
 	fn : _ <- getArgs
 	cnt <- BS.readFile fn
-	check sepTag [cnt]
-
-notEmpty :: XmlEvent -> Bool
-notEmpty (XECharData cd) = not $ BS.all isSpace cd
-notEmpty _ = True
-
-convert :: Monad m => (a -> b) -> Pipe a b m ()
-convert f = await >>= maybe (return ()) (\x -> yield (f x) >> convert f)
-
-filterP :: Monad m => (a -> Bool) -> Pipe a a m ()
-filterP p = await >>=
-	maybe (return ()) (\x -> (if p x then yield x else return ()) >> filterP p)
-
-check :: (Monad m, MonadIO m) =>
-	Pipe BS.ByteString BS.ByteString m () -> [BS.ByteString] -> m ()
-check p bss = do
-	mu <- runPipe $ fromList bss =$= p
-		=$= convert parseXmlEvent
-		=$= filterP (maybe True notEmpty)
-		=$= puts
+	mu <- runPipe $ fromList [cnt] =$= xmlEvent =$= puts
 	case mu of
 		Just _ -> return ()
 		_ -> error "bad"
