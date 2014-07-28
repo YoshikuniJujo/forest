99a43953acf414d0d25153611c8db76b541a2fe5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-07-28 15:55:07 +0900

    #410 TLSに対応した

--- subprojects/xmpipe/tls.hs
+++ subprojects/xmpipe/tls.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings, PackageImports #-}
 
+import Control.Applicative
 import Control.Arrow
 import Control.Monad
 import "monads-tf" Control.Monad.Trans
@@ -8,6 +9,9 @@ import Data.Pipe
 import Data.HandleLike
 import Text.XML.Pipe
 import Network
+import Network.PeyoTLS.Client
+import Network.PeyoTLS.ReadFile
+import "crypto-random" Crypto.Random
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
@@ -28,7 +32,28 @@ sender, recipient :: BS.ByteString
 main :: IO ()
 main = do
 	h <- connectTo "localhost" (PortNumber 54492)
-	xmpp h
+	BS.hPut h $ xmlString begin
+	BS.hPut h $ xmlString startTls
+	void . runPipe $ handleP h
+		=$= xmlEvent
+		=$= convert fromJust
+		=$= (xmlBegin >>= xmlNodeUntil isProceed)
+		=$= printP h
+	ca <- readCertificateStore ["cacert.sample_pem"]
+	g <- cprgCreate <$> createEntropyPool :: IO SystemRNG
+	(`run` g) $ do
+		p <- open' h "localhost" ["TLS_RSA_WITH_AES_128_CBC_SHA"] [] ca
+		xmpp p
+
+startTls :: [XmlNode]
+startTls = [
+	XmlNode (("", Nothing), "starttls")
+		[("", "urn:ietf:params:xml:ns:xmpp-tls")] [] [] ]
+
+isProceed :: XmlNode -> Bool
+isProceed (XmlNode ((_, Just "urn:ietf:params:xml:ns:xmpp-tls"), "proceed") _ [] [])
+	= True
+isProceed _ = False
 
 xmpp :: HandleLike h => h -> HandleMonad h ()
 xmpp h = do
