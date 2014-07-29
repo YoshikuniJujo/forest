2500cf7e03ef3d191c2a0c674e3e2bb22c738a5d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-07-29 12:05:12 +0900

    #411 サーバからクライアントにmessageを送ることに成功

--- subprojects/xmpipe/notlssv.hs
+++ subprojects/xmpipe/notlssv.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE OverloadedStrings, TupleSections, PackageImports #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies, TupleSections, FlexibleContexts,
+	PackageImports #-}
 
+import Control.Arrow
 import Control.Monad
-import "monads-tf" Control.Monad.Trans
+import "monads-tf" Control.Monad.State
 import Control.Concurrent (forkIO)
 import Data.Maybe
 import Data.Pipe
@@ -15,39 +17,119 @@ import qualified Data.ByteString.Base64 as B64
 
 import DigestSv
 
+data SHandle s h = SHandle h
+
+instance HandleLike h => HandleLike (SHandle s h) where
+	type HandleMonad (SHandle s h) = StateT s (HandleMonad h)
+	type DebugLevel (SHandle s h) = DebugLevel h
+	hlPut (SHandle h) = lift . hlPut h
+	hlGet (SHandle h) = lift . hlGet h
+	hlClose (SHandle h) = lift $ hlClose h
+	hlDebug (SHandle h) = (lift .) . hlDebug h
+
 main :: IO ()
 main = do
 	socket <- listenOn $ PortNumber 5222
 	forever $ do
 		(h, _, _) <- accept socket
-		voidM . forkIO $ xmpp h
+		voidM . forkIO . (`evalStateT` (0 :: Int)) . xmpp $ SHandle h
 
-xmpp :: HandleLike h => h -> HandleMonad h ()
+type XmppState = Int
+
+xmpp :: (MonadState (HandleMonad h), StateType (HandleMonad h) ~ Int,
+		HandleLike h) =>
+	h -> HandleMonad h ()
 xmpp h = do
 	hlPut h . xmlString $ begin ++ authFeatures
 	hlPut h . xmlString $ challengeXml
 	voidM . runPipe $ handleP h
 		=$= xmlEvent
 		=$= convert fromJust
-		=$= (xmlBegin >>= xmlNode)
+--		=$= (xmlBegin >>= xmlNode)
+		=$= xmlPipe
 		=$= convert showResponse
 		=$= processResponse h
 		=$= printP h
 
+xmlPipe :: Monad m => Pipe XmlEvent XmlNode m ()
+xmlPipe = do
+	c <- xmlBegin >>= xmlNode
+	when c $ xmlPipe
+
 data ShowResponse
-	= SRResponse BS.ByteString
+	= SRStream [(Tag, BS.ByteString)]
+	| SRAuth [(Tag, BS.ByteString)]
+	| SRResponse BS.ByteString
 	| SRResponseNull
+	| SRIq [(Tag, BS.ByteString)] [Iq]
+	| SRPresence [(Tag, BS.ByteString)] [XmlNode]
 	| SRRaw XmlNode
 	deriving Show
 
+data Iq	= IqBind [XmlNode]
+	| IqBindReq Requirement Bind
+	| IqSession
+	| IqRoster
+	| IqRaw XmlNode
+	deriving Show
+
+toIq :: XmlNode -> Iq
+toIq (XmlNode ((_, Just "urn:ietf:params:xml:ns:xmpp-bind"), "bind") _ [] [n, n'])
+	| Just r <- toRequirement n = IqBindReq r $ toBind n'
+toIq (XmlNode ((_, Just "urn:ietf:params:xml:ns:xmpp-session"), "session") _ [] [])
+	= IqSession
+toIq (XmlNode ((_, Just "jabber:iq:roster"), "query") _ [] []) = IqRoster
+toIq n = IqRaw n
+
+data Requirement = Optional | Required deriving Show
+
+toRequirement :: XmlNode -> Maybe Requirement
+toRequirement (XmlNode (_, "optional") _ [] []) = Just Optional
+toRequirement (XmlNode (_, "required") _ [] []) = Just Required
+toRequirement _ = Nothing
+
+data Bind
+	= Resource BS.ByteString
+	| BindRaw XmlNode
+	deriving Show
+
+toBind :: XmlNode -> Bind
+toBind (XmlNode ((_, Just "urn:ietf:params:xml:ns:xmpp-bind"), "resource") [] []
+	[XmlCharData cd]) = Resource cd
+toBind n = BindRaw n
+
 showResponse :: XmlNode -> ShowResponse
+showResponse (XmlStart ((_, Just "http://etherx.jabber.org/streams"), "stream") _
+	as) = SRStream $ map (first toTag) as
+showResponse (XmlNode ((_, Just "urn:ietf:params:xml:ns:xmpp-sasl"), "auth")
+	_ as []) = SRAuth $ map (first toTag) as
 showResponse (XmlNode ((_, Just "urn:ietf:params:xml:ns:xmpp-sasl"), "response")
 	_ [] []) = SRResponseNull
 showResponse (XmlNode ((_, Just "urn:ietf:params:xml:ns:xmpp-sasl"), "response")
 	_ [] [XmlCharData cd]) = SRResponse . (\(Right s) -> s) $ B64.decode cd
+showResponse (XmlNode ((_, Just "jabber:client"), "iq")
+	_ as ns) = SRIq (map (first toTag) as) (map toIq ns)
+showResponse (XmlNode ((_, Just "jabber:client"), "presence")
+	_ as ns) = SRPresence (map (first toTag) as) ns
 showResponse n = SRRaw n
 
-processResponse :: HandleLike h =>
+data Tag
+	= To | Lang | Version | Mechanism | Id | Type
+	| TagRaw QName
+	deriving (Eq, Show)
+
+toTag :: QName -> Tag
+toTag ((_, Just "jabber:client"), "to") = To
+toTag (("xml", Nothing), "lang") = Lang
+toTag ((_, Just "jabber:client"), "version") = Version
+toTag ((_, Just "urn:ietf:params:xml:ns:xmpp-sasl"), "mechanism") = Mechanism
+toTag ((_, Just "jabber:client"), "id") = Id
+toTag ((_, Just "jabber:client"), "type") = Type
+toTag n = TagRaw n
+
+processResponse ::
+	(MonadState (HandleMonad h), StateType (HandleMonad h) ~ XmppState,
+		HandleLike h) =>
 	h -> Pipe ShowResponse ShowResponse (HandleMonad h) ()
 processResponse h = do
 	mr <- await
@@ -55,13 +137,50 @@ processResponse h = do
 		Just r -> lift (procR h r) >> yield r >> processResponse h
 		_ -> return ()
 
-procR :: HandleLike h => h -> ShowResponse -> HandleMonad h ()
+procR :: (MonadState (HandleMonad h), StateType (HandleMonad h) ~ XmppState,
+		HandleLike h) =>
+	h -> ShowResponse -> HandleMonad h ()
 procR h (SRResponse _) = do
 	let sret = B64.encode . fromJust . lookup "response" $
 		responseToKvs False sampleDR
 	hlPut h . xmlString . (: []) $ XmlNode (nullQ "challenge")
 		[("", "urn:ietf:params:xml:ns:xmpp-sasl")] [] [XmlCharData sret]
 	hlDebug h "critical" $ sret `BS.append` "\n"
+procR h SRResponseNull = hlPut h . xmlString . (: []) $ XmlNode (nullQ "success")
+	[("", "urn:ietf:params:xml:ns:xmpp-sasl")] [] []
+procR h (SRStream _) = do
+	n <- get
+	modify (+ 1)
+	hlDebug h "critical" . BSC.pack . (++ "\n") $ show n
+	when (n == 1) . hlPut h . xmlString $ begin' ++ capsFeatures
+--	hlPut h $ xmlString begin'
+	return ()
+procR h (SRIq [(Id, i), (Type, "set")] [IqBindReq Required (Resource _n)]) = do
+	hlPut h . xmlString . (: []) $ XmlNode (nullQ "iq") []
+		[(nullQ "id", i), (nullQ "type", "result")]
+		[XmlNode (nullQ "jid") [] [] [XmlCharData "yoshikuni@localhost/profanity"]]
+procR h (SRIq [(Id, i), (Type, "set")] [IqSession]) = do
+	hlPut h . xmlString . (: []) $ XmlNode (nullQ "iq") []
+		[	(nullQ "id", i),
+			(nullQ "type", "result"),
+			(nullQ "to", "yoshikuni@localhost/profanity")
+			]
+		[]
+procR h (SRIq [(Id, i), (Type, "get")] [IqRoster]) = do
+	hlPut h . xmlString . (: []) $ XmlNode (nullQ "iq") []
+		[	(nullQ "id", i),
+			(nullQ "type", "result"),
+			(nullQ "to", "yoshikuni@localhost/profanity")
+			]
+		[XmlNode (nullQ "query") [("", "jabber:iq:roster")]
+			[(nullQ "ver", "1")] []]
+procR h (SRPresence _ _) =
+	hlPut h . xmlString . (: []) $ XmlNode (nullQ "message") []
+		[	(nullQ "type", "chat"),
+			(nullQ "to", "yoshikuni@localhost"),
+			(nullQ "from", "yoshio@localhost/profanity"),
+			(nullQ "id", "hoge") ]
+		[XmlNode (nullQ "body") [] [] [XmlCharData "Hogeru"]]
 procR _ _ = return ()
 
 handleP :: HandleLike h => h -> Pipe () BS.ByteString (HandleMonad h) ()
@@ -83,7 +202,7 @@ voidM = (>> return ())
 nullQ :: BS.ByteString -> QName
 nullQ = (("", Nothing) ,)
 
-begin :: [XmlNode]
+begin, begin' :: [XmlNode]
 begin = [
 	XmlDecl (1, 0),
 	XmlStart (("stream", Nothing), "stream")
@@ -94,9 +213,50 @@ begin = [
 			(nullQ "version", "1.0"),
 			((("xml", Nothing), "lang"), "en") ]
 	]
+begin' = [
+	XmlDecl (1, 0),
+	XmlStart (("stream", Nothing), "stream")
+		[	("", "jabber:client"),
+			("stream", "http://etherx.jabber.org/streams") ]
+		[	(nullQ "id", "5b5b55ce-8a9c-4879-b4eb-0231b25a54a4"),
+			(nullQ "from", "localhost"),
+			(nullQ "version", "1.0"),
+			((("xml", Nothing), "lang"), "en") ]
+	]
 
 authFeatures :: [XmlNode]
 authFeatures = [XmlNode (("stream", Nothing), "features") [] [] [mechanisms]]
+
+capsFeatures :: [XmlNode]
+capsFeatures = (: []) $ XmlNode (("stream", Nothing), "features") [] []
+	[caps, rosterver, bind, session]
+
+caps :: XmlNode
+caps = XmlNode (nullQ "c")
+	[("", "http://jabber.org/protocol/caps")]
+	[	(nullQ "hash", "sha-1"),
+		(nullQ "ver", "k07nuHawZqmndRtf3ZfBm54FwL0="),
+		(nullQ "node", "http://prosody.im")
+		]
+	[]
+
+rosterver :: XmlNode
+rosterver = XmlNode (nullQ "ver")
+	[("", "urn:xmpp:features:rosterver")]
+	[]
+	[XmlNode (nullQ "optional") [] [] []]
+
+bind :: XmlNode
+bind = XmlNode (nullQ "bind")
+	[("", "urn:ietf:params:xml:ns:xmpp-bind")]
+	[]
+	[XmlNode (nullQ "required") [] [] []]
+
+session :: XmlNode
+session = XmlNode (nullQ "session")
+	[("", "urn:ietf:params:xml:ns:xmpp-session")]
+	[]
+	[XmlNode (nullQ "optional") [] [] []]
 
 mechanisms :: XmlNode
 mechanisms = XmlNode (nullQ "mechanisms")
