b7582ea0d97338a0b8809f0a3b85dbf0d8196018
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-28 12:56:18 +0900

    #439 unsafeInterleaveIOの例を追加

--- subprojects/xml-push/try/generalizedUnsafe.hs
+++ subprojects/xml-push/try/generalizedUnsafe.hs
@@ -1,8 +1,10 @@
 {-# LANgUAGE FlexibleContexts, PackageImports #-}
 
+import Control.Applicative
 import "monads-tf" Control.Monad.State
 import qualified System.IO.Unsafe as U
 import Control.Monad.Trans.Control
+import Data.Time
 
 unsafeInterleaveIO :: MonadBaseControl IO m => m a -> m a
 unsafeInterleaveIO m = control $ \runInIO -> U.unsafeInterleaveIO (runInIO m)
@@ -12,12 +14,16 @@ m1 >>> m2 = unsafeInterleaveIO m1 >> m2
 
 example :: StateT [Int] IO ()
 example = unsafeInterleaveIO $
-	push 3 >>> push 33 >>> push 95 >>> push 66 >>> push 72
+	push 3 >> push 33 >> push 95 >> push 66 >> push 72
 
 push :: Int -> StateT [Int] IO ()
 push i = do
 	lift . putStrLn $ "push " ++ show i
 	modify (i :)
 
--- example2 :: StateT [Int] IO ()
--- example2 = unsafeInterleaveIO $
+example2 :: StateT Int IO [(Int, UTCTime)]
+example2 = unsafeInterleaveIO $ do
+	i <- get
+	modify succ
+	t <- lift $ getCurrentTime
+	((i, t) :) <$> example2
