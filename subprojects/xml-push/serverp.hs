13a6e6ded933a09c6a19be611d73fd767458d341
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-29 18:10:00 +0900

    #439 serverp.hsをクライアントの文字列をcamel caseにして返すサーバとした

--- subprojects/xml-push/serverp.hs
+++ subprojects/xml-push/serverp.hs
@@ -3,9 +3,10 @@
 import Control.Monad
 import "monads-tf" Control.Monad.Trans
 import Control.Concurrent
+import Data.Char
 import Data.Pipe
+import Data.Pipe.ByteString
 import System.IO
-import System.Environment
 import Network
 import Network.TigHTTP.Server
 import Network.TigHTTP.Types
@@ -15,18 +16,22 @@ import qualified Data.ByteString.Lazy as LBS
 
 main :: IO ()
 main = do
-	as <- getArgs
 	soc <- listenOn $ PortNumber 80
 	forever $ do
 		(h, _, _) <- accept soc
-		void . forkIO . forever $ run h as
+		void . forkIO . forever $ run h
 
-run h as = do
+run :: Handle -> IO ()
+run h = do
 	r <- getRequest h
 	print $ requestPath r
-	void . runPipe $ requestBody r =$= printP `finally` putStrLn ""
-	putResponse h . (response :: LBS.ByteString -> Response Pipe Handle)
-		. LBS.fromChunks $ map BSC.pack as
+	bd <- toLazy $ requestBody r =$= convert capitalize
+	putResponse h $ (response :: LBS.ByteString -> Response Pipe Handle) bd
+
+capitalize :: BSC.ByteString -> BSC.ByteString
+capitalize w = case BSC.uncons w of
+	Just (h, t) -> toUpper h `BSC.cons` BSC.map toLower t
+	Nothing -> w
 
 printP :: MonadIO m => Pipe BSC.ByteString () m ()
 printP = await >>= maybe (return ()) (\s -> liftIO (BSC.putStr s) >> printP)
