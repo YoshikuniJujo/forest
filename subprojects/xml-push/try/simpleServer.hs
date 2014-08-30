96c4f7bc1d161359b9e8021242f26aa6d047ac09
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-30 09:09:56 +0900

    #443 単純なサーバのコードをコピー

---
+++ subprojects/xml-push/try/simpleServer.hs
@@ -0,0 +1,25 @@
+import Control.Monad
+import Control.Concurrent
+import Data.Pipe
+import System.IO
+import System.Environment
+import Network
+import Network.TigHTTP.Server
+import Network.TigHTTP.Types
+
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as LBS
+
+main :: IO ()
+main = do
+	as <- getArgs
+	soc <- listenOn $ PortNumber 80
+	forever $ do
+		(h, _, _) <- accept soc
+		void . forkIO . forever $ do
+			req <- getRequest h
+			print $ requestPath req
+			putResponse h
+				. (response ::
+					LBS.ByteString -> Response Pipe Handle)
+				. LBS.fromChunks $ map BSC.pack as
