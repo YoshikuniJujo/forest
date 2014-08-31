775af66c53dcb0a9a6704e6af9fcc8e183543f33
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-31 18:20:15 +0900

    #443 pushEでgenerate関数を作成した

--- subprojects/xml-push/pushE.hs
+++ subprojects/xml-push/pushE.hs
@@ -25,12 +25,12 @@ import qualified Data.ByteString.Lazy as LBS
 class XmlPusher xp where
 	type PusherArg xp
 	generate :: (HandleLike h, MonadBaseControl IO (HandleMonad h)
-		) => h -> PusherArg xp -> HandleMonad h (xp h)
+		) => h -> h -> PusherArg xp -> HandleMonad h (xp h)
 	readFrom :: HandleLike h => xp h -> Pipe () XmlNode (HandleMonad h) ()
 	writeTo :: HandleLike h =>
 		xp h -> Pipe (Maybe XmlNode) () (HandleMonad h) ()
 
-data HttpPush = HttpPush {
+data HttpPush h = HttpPush {
 	needReply :: TVar Bool,
 	clientReadChan :: TChan XmlNode,
 	clientWriteChan :: TChan XmlNode,
@@ -38,16 +38,25 @@ data HttpPush = HttpPush {
 	serverWriteChan :: TChan XmlNode
 	}
 
+instance XmlPusher HttpPush where
+	type PusherArg HttpPush = ()
+	generate ch sh () = mkHttpPush ch sh
+
+mkHttpPush :: (
+	HandleLike h, MonadBaseControl IO (HandleMonad h)
+	) => h -> h -> HandleMonad h (HttpPush h)
+mkHttpPush ch sh = do
+	v <- liftBase . atomically $ newTVar False
+	(ci, co) <- clientC ch
+	(si, so) <- talk sh
+	return $ HttpPush v ci co si so
+
 main :: IO ()
 main = do
-	HttpPush _ cinc cotc sinc sotc <- do
-		v <- atomically $ newTVar False
-		ch <- connectTo "localhost" $ PortNumber 80
-		(ci, co) <- clientC ch
-		soc <- listenOn $ PortNumber 8080
-		(sh, _, _) <- accept soc
-		(si, so) <- talk sh
-		return $ HttpPush v ci co si so
+	ch <- connectTo "localhost" $ PortNumber 80
+	soc <- listenOn $ PortNumber 8080
+	(sh, _, _) <- accept soc
+	HttpPush _ cinc cotc sinc sotc <- generate ch sh ()
 	void . forkIO . runPipe_ $ fromTChan cinc
 		=$= convert (xmlString . (: []))
 		=$= printP
@@ -58,34 +67,38 @@ main = do
 		=$= toTChan cotc
 	runPipe_ $ fromTChan sinc =$= (toTChan sotc :: Pipe XmlNode () IO ())
 
-talk :: Handle -> IO (TChan XmlNode, TChan XmlNode)
+talk :: (HandleLike h, MonadBaseControl IO (HandleMonad h)
+	) => h -> HandleMonad h (TChan XmlNode, TChan XmlNode)
 talk h = do
-	inc <- atomically newTChan
-	otc <- atomically newTChan
-	void . forkIO . runPipe_ . forever $ do
+	inc <- liftBase $ atomically newTChan
+	otc <- liftBase $ atomically newTChan
+	void . liftBaseDiscard forkIO . runPipe_ . forever $ do
 		req <- lift $ getRequest h
-		lift . print $ requestPath req
+		lift . liftBase . print $ requestPath req
 		requestBody req
 			=$= xmlEvent
 			=$= convert fromJust
 			=$= xmlNode []
 			=$= toTChan inc
 		fromTChan otc =$= await >>= maybe (return ()) (\n ->
-			lift . putResponse h
-				. (response :: LBS.ByteString ->
-					Response Pipe Handle)
+			lift . putResponse h . responseP
 				$ LBS.fromChunks [xmlString [n]])
 	return (inc, otc)
 
-clientC :: Handle -> IO (TChan XmlNode, TChan XmlNode)
+responseP :: HandleLike h => LBS.ByteString -> Response Pipe h
+responseP = response
+
+clientC :: (HandleLike h, MonadBaseControl IO (HandleMonad h)
+	) => h -> HandleMonad h (TChan XmlNode, TChan XmlNode)
 clientC h = do
-	inc <- atomically newTChan
-	otc <- atomically newTChan
-	void . forkIO . runPipe_ $
+	inc <- liftBase $ atomically newTChan
+	otc <- liftBase $ atomically newTChan
+	void . liftBaseDiscard forkIO . runPipe_ $
 		fromTChan otc =$= clientLoop h =$= toTChan inc
 	return (inc, otc)
 
-clientLoop :: Handle -> Pipe XmlNode XmlNode IO ()
+clientLoop :: (HandleLike h, MonadBaseControl IO (HandleMonad h)
+	) => h -> Pipe XmlNode XmlNode (HandleMonad h) ()
 clientLoop h = (await >>=) . maybe (return ()) $ \n -> do
 	r <- lift . request h $ post "localhost" 80 "/"
 				(Nothing, LBS.fromChunks [xmlString [n]])
