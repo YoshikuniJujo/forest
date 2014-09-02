c8b66a952c02649280b1f67a2ec2555c702c1e3e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-09-02 11:11:25 +0900

    #451 XmlPusher.hs: 書き込みに返答が必要かどうかのBool値を追加した
    trialのコードがいくつか追加されている

--- subprojects/xml-push/XmlPusher.hs
+++ subprojects/xml-push/XmlPusher.hs
@@ -1,13 +1,21 @@
 {-# LANGUAGE TypeFamilies, FlexibleContexts #-}
 
 module XmlPusher (
-	XmlPusher(..)
+	XmlPusher(..), SimplePusher,
 	) where
 
+import Prelude hiding (filter)
+
+import Control.Monad
 import Control.Monad.Base
 import Control.Monad.Trans.Control
+import Control.Concurrent
+import Data.Maybe
 import Data.HandleLike
 import Data.Pipe
+import Data.Pipe.Flow
+import Data.Pipe.ByteString
+import System.IO
 import Text.XML.Pipe
 import Network.PeyoTLS.Client
 
@@ -19,4 +27,38 @@ class XmlPusher xp where
 	readFrom :: (HandleLike h, MonadBase IO (HandleMonad h)) =>
 		xp h -> Pipe () XmlNode (HandleMonad h) ()
 	writeTo :: (HandleLike h, MonadBase IO (HandleMonad h)) =>
-		xp h -> Pipe (Maybe XmlNode) () (HandleMonad h) ()
+		xp h -> Pipe (Maybe (XmlNode, Bool)) () (HandleMonad h) ()
+
+data SimplePusher h = SimplePusher
+	(Pipe () XmlNode (HandleMonad h) ())
+	(Pipe (Maybe (XmlNode, Bool)) () (HandleMonad h) ())
+
+data Zero a = Zero deriving Show
+
+instance XmlPusher SimplePusher where
+	type PusherArg SimplePusher = ()
+	type NumOfHandle SimplePusher = Zero
+	generate = const $ const simplePusher
+
+simplePusher :: MonadBaseControl IO (HandleMonad h) =>
+	HandleMonad h (SimplePusher h)
+simplePusher = return $ SimplePusher readXml writeXml
+
+readXml :: MonadBase IO m => Pipe () XmlNode m ()
+readXml = fromHandle stdin
+	=$= xmlEvent
+	=$= convert fromJust
+	=$= (xmlNode [] >> return ())
+
+writeXml :: MonadBase IO m => Pipe (Maybe (XmlNode, Bool))  () m ()
+writeXml = filter isJust
+	=$= convert (xmlString . (: []) . fst . fromJust)
+	=$= toHandle stdout
+
+testPusher :: XmlPusher xp => xp Handle ->
+	NumOfHandle xp Handle -> PusherArg xp -> IO ()
+testPusher tp hs as = do
+	xp <- generate hs as >>= return . (`asTypeOf` tp)
+	void . forkIO . runPipe_ $ readFrom xp
+		=$= convert (xmlString . (: []))
+		=$= toHandle stdout
