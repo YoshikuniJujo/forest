3f0a54992e2a347a2ccdb61104608e5fb8127220
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-09-02 16:58:06 +0900

    #455 HttpPush型をXmlPusherのインスタンスにした

--- subprojects/xml-push/HttpPush.hs
+++ subprojects/xml-push/HttpPush.hs
@@ -1,18 +1,30 @@
-{-# LANGUAGE TupleSections, TypeFamilies, FlexibleContexts,
+{-# LANGUAGE OverloadedStrings, TupleSections, TypeFamilies, FlexibleContexts,
 	PackageImports #-}
 
 module HttpPush (
-	HttpPush,
+	HttpPush, Two(..), testPusher,
 	) where
 
+import Prelude hiding (filter)
+
 import Control.Applicative
+import Control.Monad
 import "monads-tf" Control.Monad.Trans
 import Control.Monad.Base
+import Control.Monad.Trans.Control
+import Control.Concurrent hiding (yield)
 import Control.Concurrent.STM
+import Data.Maybe
 import Data.HandleLike
 import Data.Pipe
+import Data.Pipe.Flow
 import Data.Pipe.TChan
 import Text.XML.Pipe
+import Network.TigHTTP.Client
+import Network.TigHTTP.Server
+import Network.TigHTTP.Types
+
+import qualified Data.ByteString.Lazy as LBS
 
 import XmlPusher
 
@@ -40,13 +52,57 @@ setNeedReply :: MonadBase IO m => TVar Bool -> Pipe (a, Bool) a m ()
 setNeedReply nr = await >>= maybe (return ()) (\(x, b) ->
 	lift (liftBase . atomically $ writeTVar nr b) >> yield x >> setNeedReply nr)
 
-makeHttpPush :: MonadBase IO (HandleMonad h) => h -> h -> HandleMonad h (HttpPush h)
+makeHttpPush :: (HandleLike h, MonadBaseControl IO (HandleMonad h)) =>
+	h -> h -> HandleMonad h (HttpPush h)
 makeHttpPush ch sh = do
 	v <- liftBase . atomically $ newTVar False
 	(ci, co) <- clientC ch
 	(si, so) <- talk sh
 	return $ HttpPush v ci co si so
 
-clientC = undefined
+clientC :: (HandleLike h, MonadBaseControl IO (HandleMonad h)) =>
+	h -> HandleMonad h (TChan (XmlNode, Bool), TChan (Maybe XmlNode))
+clientC h = do
+	inc <- liftBase $ atomically newTChan
+	otc <- liftBase $ atomically newTChan
+	void . liftBaseDiscard forkIO . runPipe_ $ fromTChan otc
+		=$= filter isJust
+		=$= convert fromJust
+		=$= clientLoop h
+		=$= convert (, False)
+		=$= toTChan inc
+	return (inc, otc)
 
-talk = undefined
+clientLoop :: (HandleLike h, MonadBaseControl IO (HandleMonad h)) =>
+	h -> Pipe XmlNode XmlNode (HandleMonad h) ()
+clientLoop h = (await >>=) . maybe (return ()) $ \n -> do
+	r <- lift . request h $ post "localhost" 80 "/"
+		(Nothing, LBS.fromChunks [xmlString [n]])
+	return ()
+		=$= responseBody r
+		=$= xmlEvent
+		=$= convert fromJust
+		=$= (xmlNode [] >> return ())
+	clientLoop h
+
+talk :: (HandleLike h, MonadBaseControl IO (HandleMonad h)) =>
+	h -> HandleMonad h (TChan (XmlNode, Bool), TChan (Maybe XmlNode))
+talk h = do
+	inc <- liftBase $ atomically newTChan
+	otc <- liftBase $ atomically newTChan
+	void . liftBaseDiscard forkIO . runPipe_ . forever $ do
+		req <- lift $ getRequest h
+		requestBody req
+			=$= xmlEvent
+			=$= convert fromJust
+			=$= xmlNode []
+			=$= convert (, True)
+			=$= toTChan inc
+		fromTChan otc =$= await >>= maybe (return ()) (\mn ->
+			lift . putResponse h . responseP $ case mn of
+				Just n -> LBS.fromChunks [xmlString [n]]
+				_ -> "")
+	return (inc, otc)
+
+responseP :: HandleLike h => LBS.ByteString -> Response Pipe h
+responseP = response
---
+++ subprojects/xml-push/testHttpPushT.hs
@@ -0,0 +1,13 @@
+import Control.Monad
+import System.IO
+import Network
+
+import HttpPush
+
+main :: IO ()
+main = do
+	soc <- listenOn $ PortNumber 80
+	forever  $ do
+		(sh, _, _) <- accept soc
+		ch <- connectTo "localhost" $ PortNumber 8080
+		testPusher (undefined :: HttpPush Handle) (Two ch sh) ()
